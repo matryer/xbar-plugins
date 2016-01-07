ee03ce6f92d226641e565cbcaf72736026981dde
Manoj <manojlds@gmail.com>
2016-01-07 14:25:05 +0530

    Add docker status plugin

---
+++ Dev/Docker/docker-status.1m.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+#
+# Docker plugin
+# by Manoj Mahalingam (@manojlds)
+#
+# Displays the status of docker machines and running containers
+
+
+export PATH='/usr/local/bin:/usr/bin:$PATH'
+echo "⚓️"
+echo "---"
+
+DOCKER_MACHINES="$(docker-machine ls -q)"
+if [ -z "$DOCKER_MACHINES" ]; then
+  echo "No docker machine found"
+  exit 0
+fi
+
+echo "${DOCKER_MACHINES}" | while read -r machine; do
+  STATUS=$(docker-machine status $machine)
+  COLOR="red"
+  if [[ "$STATUS" == "Running" ]]; then
+    echo "$machine | color=green"
+    eval $(docker-machine env $machine)
+    CONTAINERS="$(docker ps --format "{{.Names}} ({{.Image}})")"
+    if [ -z "$CONTAINERS" ]; then
+      echo "No running containers"
+    else
+      echo "${CONTAINERS}" | while read -r container; do
+        echo "🆙 $container | color=black"
+      done
+    fi
+  else
+    echo "$machine (not running)"
+  fi
+  echo "---"
+done
