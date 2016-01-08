f9f95fdd2d262800664e79a6ba8a83da39eb4672
Pavel Popov <pavelpopov@outlook.com>
2016-01-08 21:45:23 +0300

    Fix missing newline

--- Finance/rbc.10m.sh
+++ Finance/rbc.10m.sh
@@ -10,7 +10,8 @@
 # They should be added in PATH
 
 export PATH=/usr/local/bin:~/Applications/go/bin:$PATH
-json=$(curl -s -L http://rbc.ru | pup '.indicators__group json{}') out="`echo $json | jq -r '.[] | .children | .[1:] | .[] as $c | "\($c.children | map(.text) | join(" ")) | color=\(if $c.children | .[0] | .text == "▲" then "green" else "red" end) | href=\($c.href)"'`"
+json=$(curl -s -L http://rbc.ru | pup '.indicators__group json{}')
+out="`echo $json | jq -r '.[] | .children | .[1:] | .[] as $c | "\($c.children | map(.text) | join(" ")) | color=\(if $c.children | .[0] | .text == "▲" then "green" else "red" end) | href=\($c.href)"'`"
 echo "$out" | grep Brent
 echo "---"
 echo "$out" | grep -v Brent | grep -v EUR/USD | grep USD | sed 's/color=green/color=111/;s/color=red/color=green/;s/color=111/color=red/'
