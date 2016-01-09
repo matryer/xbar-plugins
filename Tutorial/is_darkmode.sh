9b9b462944a46b0f5010654dd12610fe4ad4fc6f
Dave Wikoff <dgsims@gmail.com>
2016-01-09 18:00:58 -0500

    Adding tutorial plugin to determine if Dark Mode is enabled

---
+++ Tutorial/is_darkmode.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Is Dark Mode?
+# BitBar plugin
+#
+# by Dave Wikoff
+#
+# Example script showing how to let your scripts determine
+# whether Mac is in Dark Mode so you can adjust text colors
+
+if [ $BitBarDarkMode ]; then
+  # Mac has Dark Mode enabled.
+  echo "Dark"
+else
+  # Mac does not have Dark Mode
+  echo "Light"
+fi
