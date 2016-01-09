e5b159f670efaa8a6e3c4b4e8e3658211bb729c5
maxoumime <maxime.bertheau@hotmail.fr>
2016-01-09 18:16:31 +0100

    Update battery_cycles, add battery condition and max cycles

---
+++ System/Battery/battery_cycles.12h.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+# Battery Cycles by Maxime Bertheau
+# Get your battery cycles on the menu bar !
+
+# Get cycles number
+cycles=$(system_profiler SPPowerDataType | grep "Cycle Count" | awk '{print $3}')
+
+# If you want to change the emoticon before the number, here are some
+#echo "🔋 $cycles"
+#echo "♾ $cycles"
+#echo "♽ $cycles"
+
+echo "♾ $cycles"
+
+echo "---"
+
+# 1000 cycles maximum
+# Based on the cycles max number provided by Apple for all Macbooks after 2009
+# No way to get the Macbook age from script directly
+
+color=green
+if [ $cycles -gt 1000 ]; then
+    color=red
+elif [ $cycles -gt 750 ]; then
+    color=orange
+fi
+
+echo "Cycles: $cycles / 1000 | color=$color href='https://support.apple.com/en-us/HT201585'"
+
+# Get battery condition
+condition=$(system_profiler SPPowerDataType | grep "Condition" | sed -e 's/^.*: //')
+
+color=green
+if [ $condition != "Normal" ]; then
+    color=red
+fi
+
+echo "Battery condition: $condition | color=$color"
