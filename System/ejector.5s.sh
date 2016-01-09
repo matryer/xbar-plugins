d32fc6f12e347ddde468729330d1fc8773b4eaac
Carlson Orozco <carlsonorozco@gmail.com>
2016-01-09 22:06:32 +0800

    Cleanup Ejector

--- System/ejector.5s.sh
+++ System/ejector.5s.sh
@@ -9,15 +9,8 @@
 
 about="About Ejector | color=red href=https://github.com/carlsonorozco/ejector"
 
-if [ $(find /Volumes -maxdepth 1 -type d | wc -l) = "1" ]; then
-    echo "⏏ | color=gray"
-    echo '---'
-    echo $about
-    exit
-fi
-
 if [ "$1" = 'eject' ]; then
-    status=$(diskutil eject /Volumes/$2 | sed -e 's/\/Volumes\///g')
+    status=$(diskutil eject /Volumes/"$2" | sed -e 's/\/Volumes\///g')
     if [ "$status" = "" ]; then
         osascript -e "display notification \"Disk $2 failed to eject\" with title \"Ejector\""
     else
@@ -41,35 +34,40 @@ if [ "$1" = 'ejectall' ]; then
     exit
 fi
 
-total_drive=0
+total_drive=$(find /Volumes -maxdepth 1 -type d | wc -l | tr -s '[:space:]' )
+
+if [ $((total_drive)) = 1 ]; then
+    echo "⏏ | color=gray"
+    echo '---'
+    echo "$about"
+    exit
+fi
 
 echo "⏏ | color=black"
 echo '---'
 
-ls -p /Volumes | grep / | \
-{ while IFS= read -r line; do
-    drive=${line%?}
-    diskutil info /Volumes/$drive | grep -E 'File System Personality:|Volume Free Space:|Total Size:' | \
+df -Hl | grep /Volumes/ |
+while IFS= read -r line; do
+    drive=${line#*/Volumes/}
+
+    diskutil info /Volumes/"$drive" | grep -E 'File System Personality:|Volume Free Space:|Total Size:' | \
     { while IFS= read -r detail; do
         if [[ "$detail" =~ "Volume Free Space:" ]]
         then
-            free_space=$(echo $detail | grep "Volume Free Space: " | cut -d '(' -f 1)
+            free_space=$(echo "$detail" | grep "Volume Free Space:" | cut -d '(' -f 1)
         elif [[ "$detail" =~ "Total Size:" ]]
         then
-            total_size=$(echo $detail | grep "Total Size:" | cut -d '(' -f 1)
+            total_size=$(echo "$detail" | grep "Total Size:" | cut -d '(' -f 1)
         fi
     done
     echo "$drive | color=black bash=$0 param1=eject param2=$drive terminal=false"
     echo "├─ $free_space"
     echo "└─ $total_size"
     }
-    ((total_drive++))
 done
 
-if [ $total_drive -ge 2 ]; then
+if [ $((total_drive)) -ge 3 ]; then
     echo "Eject All | color=red bash=$0 param1=ejectall terminal=false"
 fi
-}
-
 echo '---'
-echo $about
+echo "$about"
