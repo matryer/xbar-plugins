7cb22613ff0fef0f3944aa46aa4255ad577e3f43
Mat Ryer <matryer@users.noreply.github.com>
2016-01-09 16:03:06 +0000

    added github username

--- Tutorial/cycle_text_and_detail.sh
+++ Tutorial/cycle_text_and_detail.sh
@@ -2,6 +2,7 @@
 
 # <bitbar.title>Cycle text and detail text</bitbar.title>
 # <bitbar.author>Mat Ryer</bitbar.author>
+# <bitbar.author.github>matryer</bitbar.author.github>
 # <bitbar.desc>Text above --- will be cycled through in the menu bar, whereas text underneath will be visible only when you open the menu.</bitbar.desc>
 # <bitbar.image></bitbar.image>
 
