69cea640da522a466e7d054e96f9f0873ae0bf18
Rob DeSanno <rdesanno@dstillery.com>
2016-01-11 15:32:53 -0500

    timing and unacknowledged host down condition added

--- Dev/Nagios/nagios.30s.sh
+++ Dev/Nagios/nagios.30s.sh
@@ -17,20 +17,22 @@ PASSWORD="{password}"				# password
 TEMP_FILE="/tmp/nagios.out"
 TAC="tac.cgi"
 STATUS="status.cgi"
-DOWN="?hostgroup=all&style=hostdetail&hoststatustypes=4&hostprops=4"
+DOWN="?hostgroup=all&style=hostdetail&hoststatustypes=4&hostprops=42"
 CRITICAL="?host=all&style=detail&servicestatustypes=16"
 WARNING="?host=all&style=detail&servicestatustypes=4"
 UNKNOWN="?host=all&style=detail&servicestatustypes=8"
+OK="?host=all&style=detail&servicestatustypes=2"
 
 curl -s -u $NAME:$PASSWORD https://$URL/nagios/cgi-bin/$TAC > $TEMP_FILE
 
-#down=`grep "$DOWN" $TEMP_FILE | grep  | cut -d\> -f3 | cut -d\< -f1`
+down=`grep "$DOWN" $TEMP_FILE | grep Down | cut -d\> -f3 | cut -d\< -f1`
 critial=`grep "$CRITICAL" $TEMP_FILE | grep Critical | cut -d\> -f3 | cut -d\< -f1`
 warning=`grep "$WARNING" $TEMP_FILE | grep Warning | cut -d\> -f3 | cut -d\< -f1`
 unknown=`grep "$UNKNOWN" $TEMP_FILE | grep Unknown | cut -d\> -f3 | cut -d\< -f1`
+ok=`grep "$OK" $TEMP_FILE | grep Ok | cut -d\> -f3 | cut -d\< -f1`
 
-# insert host down here 
-#echo "$down | color=purple href=https://$URL/nagios/cgi-bin/$STATUS/$DOWN"
+echo "$down | color=purple href=https://$URL/nagios/cgi-bin/$STATUS/$DOWN"
 echo "$critial | color=red href=https://$URL/nagios/cgi-bin/$STATUS/$CRITICAL"
-echo "$warning | color=yellow href=https://$URL/nagios/cgi-bin/$STATUS/$WARNING"
+echo "$warning | color=brown href=https://$URL/nagios/cgi-bin/$STATUS/$WARNING"
 echo "$unknown | color=orange href=https://$URL/nagios/cgi-bin/$STATUS/$UNKNOWN"
+echo "$ok | color=green href=https://$URL/nagios/cgi-bin/$STATUS/$OK"
