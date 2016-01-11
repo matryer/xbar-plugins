476562c7a7c5e4778941788dd3adfa292c929b9b
Rob DeSanno <rob@desanno.com>
2016-01-10 23:18:35 -0500

    Nagios status plugin

---
+++ Dev/Nagios/nagios.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# <bitbar.title>Nagios Prod</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Rob DeSanno</bitbar.author>
+# <bitbar.author.github>rdesanno</bitbar.author.github>
+# <bitbar.desc>Nagios status summary</bitbar.desc>
+# <bitbar.image>https://assets.nagios.com/images/footer/Nagios-Logo-Legal.png</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
+## set variables
+
+URL="{set your nagios url here}"  	# ie nagios.example.com
+NAME="{username}" 					# username
+PASSWORD="{password}"				# password
+
+TEMP_FILE="/tmp/nagios.out"
+TAC="tac.cgi"
+STATUS="status.cgi"
+DOWN="?hostgroup=all&style=hostdetail&hoststatustypes=4&hostprops=4"
+CRITICAL="?host=all&style=detail&servicestatustypes=16"
+WARNING="?host=all&style=detail&servicestatustypes=4"
+UNKNOWN="?host=all&style=detail&servicestatustypes=8"
+
+curl -s -u $NAME:$PASSWORD https://$URL/nagios/cgi-bin/$TAC > $TEMP_FILE
+
+#down=`grep "$DOWN" $TEMP_FILE | grep  | cut -d\> -f3 | cut -d\< -f1`
+critial=`grep "$CRITICAL" $TEMP_FILE | grep Critical | cut -d\> -f3 | cut -d\< -f1`
+warning=`grep "$WARNING" $TEMP_FILE | grep Warning | cut -d\> -f3 | cut -d\< -f1`
+unknown=`grep "$UNKNOWN" $TEMP_FILE | grep Unknown | cut -d\> -f3 | cut -d\< -f1`
+
+# insert host down here 
+#echo "$down | color=purple href=https://$URL/nagios/cgi-bin/$DOWN"
+echo "$critial | color=red href=https://$URL/nagios/cgi-bin/$CRITICAL"
+echo "$warning | color=yellow href=https://$URL/nagios/cgi-bin/$WARNING"
+echo "$unknown | color=orange href=https://$URL/nagios/cgi-bin/$UNKNOWN"
