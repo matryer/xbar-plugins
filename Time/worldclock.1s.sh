05dbcd6d54de22589cf248c23482c378d45f32ea
Adam Snodgrass <adam.snodgrass@blackboard.com>
2016-01-12 10:58:23 -0500

    Fix typos in world clock metadata

--- Time/worldclock.1s.sh
+++ Time/worldclock.1s.sh
@@ -5,9 +5,9 @@
 # the format and refresh rate to taste.
 #
 # <bitbar.title>World Clock</bitbar.title>
-# <bitbar.version>v1.0</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Adam Snodgrass</bitbar.author>
-# <bitbar.author.github>asnodgrass</bitbar.author>
+# <bitbar.author.github>asnodgrass</bitbar.author.github>
 # <bitbar.desc>Display current time in various timezones</bitbar.desc>
 # <bitbar.image>https://cloud.githubusercontent.com/assets/6187908/12207887/464ff8b2-b617-11e5-9d61-787eed228552.png</bitbar.image>
 # <bitbar.dependencies>none</bitbar.dependencies>
