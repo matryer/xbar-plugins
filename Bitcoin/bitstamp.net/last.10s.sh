d46843bf8d40aa5f39a839c04e9a7e6c5afb0c27
Damien Lajarretie <dlajarretie@airintservices.com>
2016-01-12 11:55:17 +0100

    add metadata to bitstamp plugin

--- Bitcoin/bitstamp.net/last.10s.sh
+++ Bitcoin/bitstamp.net/last.10s.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
-# Bitstamp rate
-# BitBar plugin
+# Shows last BTC price (in USD) on Bitstamp exchange.
+#
+# <bitbar.title>Bitstamp last price plugin</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Damien Lajarretie</bitbar.author>
+# <bitbar.author.github>dlajarretie</bitbar.author.github>
+# <bitbar.desc>Shows last BTC price (in USD) on Bitstamp exchange.</bitbar.desc>
+# <bitbar.image>https://bitcoin.fr/public/divers/Bitstamp.png</bitbar.image>
 #
 # by Damien Lajarretie
 # Based on Coinbase bitbar plugin by Mat Ryer
-#
-# Shows last BTC price (in USD) on Bitstamp exchange.
-#
 
 echo -n "Bitstamp: $"; curl -s "https://www.bitstamp.net/api/ticker/" | egrep -o '"last": "[0-9]+(\.)?([0-9]{0,2}")?' | sed 's/"last": //' | sed 's/\"//g'
