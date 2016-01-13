cbe117f36b4ea1eba2a57df1a8f2a0d9f4779740
Alexandre Espinosa Menor <aemenor@gmail.com>
2016-01-13 18:24:58 +0100

    Removed duplicated trackpad: incorrect refresh time and same funcionality

--- System/Battery/trackpad.1.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-PERCENTAGE=`ioreg -c BNBTrackpadDevice | grep BatteryPercent | fgrep -v { | sed 's/[^[:digit:]]//g'`
-
-if [ "$PERCENTAGE" ]; then
-        echo "Trackpad: $PERCENTAGE%"
-else
-        echo "without bluetooth trackpad?"
-fi
