cbd7599f0d7578247a2589dc42e15c4f23f3a924
Jamieson Colburn <me@jamieson.io>
2016-01-15 15:56:39 -0500

    Simplified code to eliminate if/else block

--- Finance/gfinance.5m.py
+++ Finance/gfinance.5m.py
@@ -5,7 +5,7 @@
 # <bitbar.author>Jamieson Colburn</bitbar.author>
 # <bitbar.author.github>jamiesonio</bitbar.author.github>
 # <bitbar.desc>Provides a rotating stock ticker in your menu bar</bitbar.desc>
-# <bitbar.image>http://nothingreally.botler.me/bitbar.currency-tracker.png</bitbar.image>
+# <bitbar.image>https://s3.amazonaws.com/jamieson.io/bitbar_gfinance.png</bitbar.image>
 # <bitbar.dependencies>python</bitbar.dependencies>
 import urllib2
 import json
@@ -24,7 +24,4 @@ query = u.read()
 obj = json.loads(query[4:-1])
 
 for ticker in obj:
-    if float(ticker["c"]) < 0:
-        print ticker["t"], ticker["l"], ticker["c"], "| color=red"
-    elif float(ticker["c"]) > 0:
-        print ticker["t"], ticker["l"], ticker["c"], "| color=green"
+    print "{} {} {} | color=".format(ticker["t"], ticker["l"], ticker["c"]), "red" if float(ticker["c"]) < 0 else "green"
