761598cc5ad464ed8fb639a314144d13751b8a7f
JimDeanSpivey <JimDeanSpivey@gmail.com>
2016-01-20 12:30:52 -0500

    Forgot a Less Than Sign

--- Dev/Redis/checkRedisMemory.sh
+++ Dev/Redis/checkRedisMemory.sh
@@ -2,7 +2,7 @@
 # <bitbar.title>Check remote memory usage of Redis Server</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Jimmy Spivey</bitbar.author>
-# <bitbar.author.github>JimDeanSpivey/bitbar.author.github>
+# <bitbar.author.github>JimDeanSpivey</bitbar.author.github>
 # <bitbar.desc>This will invoke SSH command locally and connect to a server that runs redis, where it will check the memory usage. You will likely run into this error: "ssh_askpass: exec(/usr/libexec/ssh-askpass): No such file or directory" You will need to install ssh_askpass found here: https://github.com/markcarver/mac-ssh-askpass to fix that and also enable to you to ssh from BitBar.</bitbar.desc>
 # <bitbar.dependencies>ssh_askpass</bitbar.dependencies>
 # <bitbar.image>http://i.imgur.com/ikCSVBq.png</bitbar.image>
