f0933e997ca2d93477b2433e800249e8eabd4741
Matteo Ferrando <matteo.ferrando2@gmail.com>
2016-01-23 00:00:18 -0430

    Fixed uptime to work with all the possible outputs
    uptime happens to have several different kinds of output, I'll list some examples:
    
    > 22:49  up 24 secs, 2 users, load averages: 8.37 2.09 0.76
        [ ↑ 24 secs ]
    
    > 22:50  up 1 min, 2 users, load averages: 5.59 2.39 0.95
        [ ↑ 1 min ]
    
    > 23:39  up 51 mins, 3 users, load averages: 2.18 1.94 1.74
        [ ↑ 51 mins ]
    
    > 23:54  up  1:06, 3 users, load averages: 3.67 2.57 2.07
        [ ↑ 1 hr, 6 mins ]
    
    > 16:20  up 120 days, 10:46, 3 users, load averages: 3.67 2.57 2.07
        [ ↑ 120 days, 10 hrs, 46 mins ]

--- System/uptime.1m.sh
+++ System/uptime.1m.sh
@@ -8,6 +8,29 @@
 # Shows details about the current uptime of the system.
 
 INFO=`uptime`
-echo $INFO | tr "," " " | awk '{print "[ up "$3" days, "$5" ]"}'
+echo $INFO | awk -F'[ ,:\t\n]+' '{
+        msg = "↑ "
+        if ($5 == "day" || $5 == "days") {      # up for a day or more
+            msg = msg $4 $5 ", "
+
+            n = $6
+            o = $7
+        } else {
+            n = $4
+            o = $5
+        }
+
+        if (int(o) == 0) {                      # words evaluate to zero
+            msg = msg int(n)" "o
+        } else {                                # hh:mm format
+            msg = msg int(n)" hr"
+            if (n > 1) { msg = msg "s" }
+
+            msg = msg ", " int(o) " min"
+            if (o > 1) { msg = msg "s" }
+        }
+
+        print "[", msg, "]"
+    }'
 echo "---"
 echo $INFO | tr "," "\n" | tail -n 2
