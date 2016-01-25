4ddd3a544319e66877bd08151ba57bdf0690094a
citoki <steffenfroehlich@gmx.de>
2016-01-25 16:55:58 +0100

    Add plugin to display week number

---
+++ Time/weeknumber.1m.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# <bitbar.title>Week Number</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author>Steffen Froehlich</bitbar.author>
+# <bitbar.author.github>citoki</bitbar.author.github>
+# <bitbar.desc>This plugin will show the current week number.</bitbar.desc>
+# <bitbar.image>https://imgur.com/vsCWLwX</bitbar.image>
+# <bitbar.dependencies></bitbar.dependencies>
+
+echo $(date +"%V")
+echo "---"
