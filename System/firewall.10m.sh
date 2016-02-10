4feb2484d04527dd79b9c8a5a8e867fcfd7621df
Eddie A Tejeda <eddie.tejeda@gmail.com>
2016-02-09 23:40:07 -0800

    add plugin to detect if firewall is enabled

---
+++ System/firewall.10m.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# <bitbar.title>Firewall</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Eddie A Tejeda</bitbar.author>
+# <bitbar.author.github>eddietejeda</bitbar.author.github>
+# <bitbar.desc>This plugin displays the status of the firewall.</bitbar.desc>
+# <bitbar.dependencies></bitbar.dependencies>
+
+enabled=$(/usr/libexec/ApplicationFirewall/socketfilterfw --getglobalstate);
+
+if [[ $enabled == *"enabled"* ]]
+then
+  echo '☵'  
+else
+  echo '❌'
+fi
+
+echo '---'
+echo 👁 $(/usr/libexec/ApplicationFirewall/socketfilterfw --getstealthmode)
