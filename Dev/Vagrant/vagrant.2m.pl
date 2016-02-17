20ae874dab9810c60d4221ede2e70140f0edbe83
axeloz <axel@mabox.eu>
2016-02-17 12:20:27 +0100

    Adding notification + SSH
    Adding new action « Connect to SSH »
    Adding notifications when action completes with success/error.

--- Dev/Vagrant/vagrant.2m.pl
+++ Dev/Vagrant/vagrant.2m.pl
@@ -15,7 +15,11 @@ my $me = abs_path($0);
 my $content;
 my $vagrant;
 my $running = 0;
+my $total = 0;
 my $path = $ENV{PATH}.':/usr/local/bin';
+
+# This function allows me to run Apple Scripts
+sub osascript($) { system 'osascript', map { ('-e', $_) } split(/\n/, $_[0]); }
 
 # Locating the Vagrant binary
 foreach $a (split(/:/, $path)) {
@@ -36,17 +40,44 @@ if (! defined $vagrant) {
 # When script is call with 2 arguments
 # $ARGV[0] : the action (up, halt, suspend, resume)
 # $ARGV[1] : the path of the Vagrant environment
-if ( ($#ARGV + 1) == 2) {
+# $ARGV[2] : the ID of the VM
+if ( ($#ARGV + 1) == 3) {
+	
+	my $title = "Vagrant machine #$ARGV[2]";
+	my $description = "";
+	my $newstatus = "unknown";
 
 	# Running the action
-	system("export PATH=$path && cd $ARGV[1] && $vagrant $ARGV[0]");
+	if ($ARGV[0] eq 'ssh') {
+		system("cd $ARGV[1] && $vagrant ssh &");
+		$description = "You are now connected to your Vagrant machine";
+	}
+	else {
+		if ($ARGV[0] eq "up" || $ARGV[0] eq "resume") {
+			$newstatus = "running";
+		}
+		elsif  ($ARGV[0] eq "halt" || $ARGV[0] eq "suspend") {
+			$newstatus = "stopped";
+		}
+		elsif ($ARGV[0] eq "reload") {
+			$newstatus = "reloaded";
+		}
+
+		system("export PATH=$path && cd $ARGV[1] && $vagrant $ARGV[0]");
+		$description = "Vagrant virtual machine status is now ".$newstatus;
+	}
+	
 	# Checking the result of the action
 	if ($? eq 0) {
-		print "OK\n";
+		&osascript (
+		 'display notification "'.$description.'" with title "'.$title.'"'
+		);
 		exit 0;
 	}
 	else {
-		print "An error occurred\n";
+		&osascript (
+		 'display notification "Could not execute operation" with title "'.$title.'"'
+		);
 		exit 1;
 	}
 	# Not needed, just safer...
@@ -65,6 +96,9 @@ foreach $a (@output) {
 
 	# Cutting output on first empty line as Vagrant is too verbose
 	last if ($a eq '');
+	
+	# Counting total
+	$total ++;
 
 	# Exploding row on spaces
 	@found = split / /, $a;
@@ -77,24 +111,25 @@ foreach $a (@output) {
 		$content .= "✅ Machine $found[0] is running | size=14 color=green\n";
 		$content .= " $found[4] | size=11 \n";
 		$content .= "  | size=14 color=black \n";
-		$content .= "🔄 Reload $found[0] | size=12 bash=\"$me\" param1=reload param2=\"".$found[4]."\" terminal=false refresh=true \n";
-		$content .= "🔽 Suspend $found[0] | size=12 bash=\"$me\" param1=suspend param2=\"".$found[4]."\" terminal=false refresh=true \n";
-		$content .= "⏬ Stop $found[0] | size=12 bash=\"$me\" param1=halt param2=\"".$found[4]."\" terminal=false refresh=true \n";
+		$content .= "#️⃣ SSH $found[0] | size=12 bash=\"$me\" param1=ssh param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=true refresh=false \n";
+		$content .= "🔄 Reload $found[0] | size=12 bash=\"$me\" param1=reload param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
+		$content .= "🔽 Suspend $found[0] | size=12 bash=\"$me\" param1=suspend param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
+		$content .= "⏬ Stop $found[0] | size=12 bash=\"$me\" param1=halt param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
 	}
 	# This VM is currently saved
 	elsif ($found[3] eq 'saved') {
 		$content .= "📴 Machine $found[0] is suspended | size=14 color=orange\n";
 		$content .= " $found[4] | size=11 \n";
 		$content .= "  | size=14 color=black \n";
-		$content .= "▶️ Resume $found[0] | size=12 bash=\"$me\" param1=resume param2=\"".$found[4]."\" terminal=false refresh=true \n";
-		$content .= "⏬ Stop $found[0] | size=12 bash=\"$me\" param1=halt param2=\"".$found[4]."\" terminal=false refresh=true \n";
+		$content .= "▶️ Resume $found[0] | size=12 bash=\"$me\" param1=resume param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
+		$content .= "⏬ Stop $found[0] | size=12 bash=\"$me\" param1=halt param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
 	}
 	# This VM is currently powered off
 	elsif ($found[3] eq 'poweroff') {
 		$content .= "🚫 Machine $found[0] is stopped | size=14 color=red\n";
 		$content .= " $found[4] | size=11 \n";
 		$content .= "  | size=14 color=black \n";
-		$content .= "▶️ Start $found[0] | size=12 bash=\"$me\" param1=up param2=\"".$found[4]."\" terminal=false refresh=true \n";
+		$content .= "▶️ Start $found[0] | size=12 bash=\"$me\" param1=up param2=\"".$found[4]."\" param3=\"".$found[0]."\" terminal=false refresh=true \n";
 	}
 	# This VM is in an unknown state
 	else {
@@ -109,7 +144,7 @@ foreach $a (@output) {
 }
 
 # Adding the menu title with the number of running VMs
-print "Vagrant [$running]\n";
+print "V [$running/$total]\n";
 print "---\n";
 print $content unless !defined $content;
 exit 0;
