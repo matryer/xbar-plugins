889d7762b6c4b19bbea0cf1c601f6588e16580dc
M. Peter <mp@tcs.de>
2016-02-19 13:11:44 +0100

    convert a unix timestamps to a date.

---
+++ Time/ts-convert.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+# <bitbar.title>Clipboard Timestamp converter</bitbar.title>
+# <bitbar.version>v0.1</bitbar.version>
+# <bitbar.author>M. Peter</bitbar.author>
+# <bitbar.author.github>mpneuried</bitbar.author.github>
+# <bitbar.desc>This plugin converts timestamps from the clipboard to a date.
+# It's inspired by the original clipboard-base64-encoder plugin</bitbar.desc>
+# <bitbar.image></birtbar.image>
+# <bitbar.dependencies></bitbar.dependencies>
+
+# Hack for language not being set properly and unicode support
+export LANG="${LANG:-en_US.UTF-8}"
+
+PREFIX=""
+APPENDIX=""
+
+
+# user clicks on 'encodepaste'
+if [[ "$1" = "encodepaste" ]]; then
+  CONVERTED=$(echo -n "${PREFIX}$(pbpaste)${APPENDIX}" | xargs -I {} date -r {} +'%Y-%m-%d %H:%M:%S')
+  $(echo -n "$CONVERTED" | pbcopy)
+  osascript -e "display notification \"$CONVERTED\" with title \"DATE:\"" &> /dev/null
+  exit
+fi
+
+# user clicks on 'encoding'
+if [[ "$1" = "encode" ]]; then
+  CONVERTED=$(echo -n "${PREFIX}$(pbpaste)${APPENDIX}" | xargs -I {} date -r {} +'%Y-%m-%d %H:%M:%S')
+  osascript -e "display notification \"$CONVERTED\" with title \"DATE:\"" &> /dev/null
+  exit
+fi
+
+# user clicks on 'now'
+if [[ "$1" = "now" ]]; then
+  NOW=$(date +%s)
+
+  $(echo -n "$NOW" | pbcopy)
+  osascript -e "display notification \"$NOW\" with title \"Save to Clipboard:\"" &> /dev/null
+  exit
+fi
+
+# Print icon
+echo "⏱"
+echo "---"
+echo "Convert clipboard timestamp to date | bash=$0 param1=encode terminal=false"
+echo "Convert clipboard timestamp to date and write it to the clipboard | bash=$0 param1=encodepaste terminal=false"
+echo "Write the current timestamp to the clipboard | bash=$0 param1=now terminal=false"
