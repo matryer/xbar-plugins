0969bf68c4ed353e4ce48effcc22c75a23bcc88b
Olivier Tille <oliviernt@users.noreply.github.com>
2016-02-22 20:57:49 +0100

    Adding image to process-monitoring plugin
    Adding image and fixed unescaped $id var in script

--- Dev/Monitoring/process-monitoring.1s.sh
+++ Dev/Monitoring/process-monitoring.1s.sh
@@ -5,7 +5,7 @@
 # <bitbar.author>Olivier Tille</bitbar.author>
 # <bitbar.author.github>oliviernt</bitbar.author.github>
 # <bitbar.desc>Monitors CPU and Memory usage for a certain process</bitbar.desc>
-# <bitbar.image></bitbar.image>
+# <bitbar.image>http://i.imgur.com/ApLSN8L.png</bitbar.image>
 # <bitbar.dependencies></bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/oliviernt/bitbar-plugins/blob/monitoring/Dev/Monitoring/process-monitoring.1s.sh</bitbar.abouturl>
 
@@ -13,6 +13,6 @@ PROCESS="BitBar.app"
 
 id=`ps aux | grep -i "${PROCESS}" | grep -v grep | awk '{print $2}'`
 echo -n "${PROCESS}: "
-top -l 1 -pid $id -stats cpu,mem | tail -1 | awk '{print "CPU: " $1 " - MEM: " $2}'
+top -l 1 -pid "$id" -stats cpu,mem | tail -1 | awk '{print "CPU: " $1 " - MEM: " $2}'
 echo "---"
-top -l 1 -pid $id
+top -l 1 -pid "$id"
