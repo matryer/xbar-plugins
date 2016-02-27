39c037a54531c1b26a640cd9bf6afade935e6e96
Srdgh <Srdgh@users.noreply.github.com>
2016-02-27 12:45:46 +0000

    Update homebrewcask.1d.sh
    stop 'brew update' output from displaying in menubar.

--- Dev/Homebrew/homebrewcask.1d.sh
+++ Dev/Homebrew/homebrewcask.1d.sh
@@ -7,17 +7,17 @@
 # <bitbar.desc>List out-of-date apps i.e. apps for whch a newer Homebrew cask exists. Click menu item to install cask.</bitbar.desc>
 # <bitbar.image>http://i.imgur.com/kBf90TB.png?1</bitbar.image>
 
-/usr/local/bin/brew update
+brewupdate=$(/usr/local/bin/brew update) # Plugin performs 'brew update' and captures output. Currently does nothing with output. Use this variable to add further information to plugin (e.g. new non-cask formulae which have been updated)
 
-brewcall=$(/usr/local/bin/brew cask ls -1 | sed 's/(!)//g' | xargs /usr/local/bin/brew cask info | grep -A 1 'Not installed' | sed -e 's_Not installed__g' -e 's_https://github\.com/caskroom/homebrew-cask/blob/master/Casks/__g' -e 's_\.rb__g')
+brewcasklist=$(/usr/local/bin/brew cask ls -1 | sed 's/(!)//g' | xargs /usr/local/bin/brew cask info | grep -A 1 'Not installed' | sed -e 's_Not installed__g' -e 's_https://github\.com/caskroom/homebrew-cask/blob/master/Casks/__g' -e 's_\.rb__g')
 
-brewnum=$(for line in $brewcall; do echo $line | grep "[a-z]" ; done | wc -w | xargs)
+brewcasknum=$(for line in $brewcasklist; do echo $line | grep "[a-z]" ; done | wc -w | xargs)
 
 if [[ "${brewnum}" != "0" ]]; then
 echo "${brewnum} updates"
 echo "---"
-echo $brewnum "casks to update"
-for line in $brewcall; do echo $line | grep "[a-z]" | sed 's/\(.*\)/Update & | bash=\"brew cask install &\" terminal=true refresh=/g' ; done
+echo $brewcasknum "casks to update"
+for line in $brewcasklist; do echo $line | grep "[a-z]" | sed 's/\(.*\)/Update & | bash=\"brew cask install &\" terminal=true refresh=/g' ; done
 echo "---"
 # Uncomment following lines to add the commands to the drop-down menu
 # echo "Brew Update | bash='brew update' terminal=true refresh="
