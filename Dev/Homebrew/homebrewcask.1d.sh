0636ec68be11bd25f0c4c2078a6c8a4e2b00c581
Srdgh <Srdgh@users.noreply.github.com>
2016-02-28 11:11:27 +0000

    Update homebrewcask.1d.sh
    brew update was yielding errors. Replaced with code from the other homebrew plugin.

--- Dev/Homebrew/homebrewcask.1d.sh
+++ Dev/Homebrew/homebrewcask.1d.sh
@@ -7,7 +7,13 @@
 # <bitbar.desc>List out-of-date apps i.e. apps for whch a newer Homebrew cask exists. Click menu item to install cask.</bitbar.desc>
 # <bitbar.image>http://i.imgur.com/kBf90TB.png?1</bitbar.image>
 
-brewupdate=$(/usr/local/bin/brew update) # performs 'brew update' and captures output. Currently does nothing with output. Use this variable to add further information to plugin (e.g. new non-cask formulae which have been updated)
+#I've taken the next six lines from the brew update plugin.
+exit_with_error() {
+  echo "err | color=red";
+  exit 1;
+}
+
+/usr/local/bin/brew update > /dev/null || exit_with_error; 
 
 brewcasklist=$(/usr/local/bin/brew cask ls -1 | sed 's_(!)__g' | xargs /usr/local/bin/brew cask info | grep -A 1 'Not installed' | sed -e 's_Not installed__g' -e 's_https://github\.com/caskroom/homebrew-cask/blob/master/Casks/__g' -e 's_\.rb__g')
 
