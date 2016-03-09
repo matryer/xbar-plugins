bc25db376a95a4e7a138b0c1d791b919dc407a3f
Raymond Kuiper <qix@the-wired.net>
2016-03-09 10:17:22 +0100

    Changes to netinfo:  - More comments  - the 'X' in the title bar is now shown if both IPv4 and IPv6 external lookups fail  - A bug in copying of interface information to clipboard was fixed  - Added a notification function  - Speedtest action now uses the notify function to inform users on status  - Copy actions are now notified to the user  - Before trying a speedtest, speedtest-cli is verified to be in the path

--- Network/netinfo.60s.sh
+++ Network/netinfo.60s.sh
@@ -8,37 +8,63 @@
 # <bitbar.dependencies>speedtest-cli</bitbar.dependencies>
 # <bitbar.image>http://i.imgur.com/zFv3RvI.png</bitbar.image>
 #
+#
+# This bitbar plugin was based on the original "external-ip" Bitbar plugin by Mat Ryer.
+# A lot of new functionality has been added, including adding support for speedtest.net and listing internal interface information.
+#
 
+# Set path and Speedtest tmp file
 PATH=/usr/local/bin:$PATH
 SPEEDTEST="/tmp/speedtest.txt"
 
+
+# Function to notify the user via Aple Script
+notify () {
+    osascript -e "display notification \"$1\" with title \"Netinfo\""
+}
+
+# If called with parameter "copy", copy the second parameter to the clipboard
 if [ "$1" = "copy" ]; then
   # Copy to clipboard
   echo "$2" | pbcopy
+  notify "Copied $2 to clipboard"
   exit 0
 fi
 
+# If called with parameter "speedtest", run speedtest-cli
 if [ "$1" = "speedtest" ]; then
-  # Perform a speedtest
-  speedtest-cli --simple --share > "$SPEEDTEST"
+  # test if speedtest-cli is found
+  if [[ "$(which speedtest-cli)" != "" ]]; then
+     # Perform a speedtest
+     speedtest-cli --simple --share > "$SPEEDTEST" && notify "Speedtest is finished" || notify "Speedtest failed"
+  else
+     notify "Speedtest-cli not found!"
+  fi
   exit 0
 fi
 
-#Get external data
+# Get external IPs
 EXTERNAL_IP4=$(curl --connect-timeout 3 -s http://v4.ipv6-test.com/api/myip.php || echo None)
 EXTERNAL_IP6=$(curl --connect-timeout 3 -s http://v6.ipv6-test.com/api/myip.php || echo None)
-INTERFACES=$(ifconfig | grep UP | egrep -o '(^en[0-9]*|^utun[0-9]*)' | sort -n)
+
+# Perform whois lookup on the external IPv4 address.
 [[ "$EXTERNAL_IP4" == "None" ]] && WHOIS="" || WHOIS=$(whois $EXTERNAL_IP4 | awk '/descr: / {$1=""; print $0 }' | head -n 1)
 
+# Find interfaces
+INTERFACES=$(ifconfig | grep UP | egrep -o '(^en[0-9]*|^utun[0-9]*)' | sort -n)
 
-# Start output
-[[ "$EXTERNAL_IP4" == "None" ]] && echo "❌" || echo "🌐"
+# Start building output
+[[ "$EXTERNAL_IP4" == "None" && "$EXTERNAL_IP6" == "None" ]]  && echo "❌" || echo "🌐"
+echo "---"
+echo "🔄 Refresh | colo=black refresh=true"
 echo "---"
 echo "Public: "
 echo "IPv4: ${EXTERNAL_IP4}${WHOIS} | terminal=false bash=$0 param1=copy param2=$EXTERNAL_IP4"
 echo "IPv6: ${EXTERNAL_IP6} | terminal=false bash=$0 param1=copy param2=$EXTERNAL_IP6"
 echo "---"
-echo "📈 Perform Speedtest | color=blue terminal=false refresh=true bash=$0 param1=speedtest"
+echo "📈 Perform Speedtest | color=back terminal=false refresh=true bash=$0 param1=speedtest"
+
+# Pretty format the last speedtest if the tmp file is found
 if [[ -e "$SPEEDTEST" ]]; then
      LAST=$(stat -f "%Sm" -t "%Y-%m-%d %H:%M:%S" "$SPEEDTEST")
      PING=$(awk '/Ping: / { $1=""; print $0 }' "$SPEEDTEST")
@@ -51,10 +77,12 @@ else
      echo "Last checked: Never"
 fi
 
+# Loop through the interfaces and output MAC, IPv4 and IPv6 information
 echo "---"
 for INT in $INTERFACES; do
      echo "$INT:"
-     ifconfig $INT | awk '/ether/ { print "MAC: " $2 "| terminal=false bash=$0 param1=copy param2=" $2 }; /inet / { print "IPv4: " $2 "| terminal=false bash=$0 param1=copy param2=" $2 };  /inet6/ { print "IPv6: " $2 "| terminal=false bash=$0 param1=copy param2=" $2 }' | sed 's/%.*\|/\|/'
-     #[[ "$IP" != "" ]] && echo "$INT: $IP | terminal=false bash=$0 param1=copy param2=$IP"
+     ifconfig $INT | awk "/ether/ { print \"MAC: \" \$2 \" | terminal=false bash=$0 param1=copy param2=\" \$2 }; /inet / { print \"IPv4: \" \$2 \" | terminal=false bash=$0 param1=copy param2=\" \$2 };  /inet6/ { print \"IPv6: \" \$2 \" | terminal=false bash=$0 param1=copy param2=\" \$2 }" | sed -e 's/%utun[0-9]*//g' -e 's/%en[0-9]*//g' | sort
      echo "---"
 done
+
+# EoF
