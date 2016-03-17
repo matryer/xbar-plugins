58dc7dd86a114aab230cc43e6d06ede101d26464
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2016-03-16 19:28:09 -0700

    Fixup printf format for checkRedisMemory.sh

--- Dev/Redis/checkRedisMemory.sh
+++ Dev/Redis/checkRedisMemory.sh
@@ -15,7 +15,7 @@ HOST="user@hostname"
 
 printf 'Redis: ' 
 RESULT=$(ssh $HOST "redis-cli INFO | grep -o '[0-9\.]*[A-Z]\s' | head -2")
-printf $RESULT | head -1
+printf "%s" "$RESULT" | head -1
 echo ---
 LINE1=$(echo "$RESULT" | head -n1)
 LINE2=$(echo "$RESULT" | tail -n1)
