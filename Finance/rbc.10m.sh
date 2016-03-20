2175e8225e13d8280aafe53229c8209f070bf04a
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2016-03-20 04:18:42 -0600

    Fix up linter issues, pup/jq output, and add metadata

--- Finance/rbc.10m.sh
+++ Finance/rbc.10m.sh
@@ -8,10 +8,17 @@
 #   pup - github.com/EricChiang/pup
 #   curl - curl.haxx.se
 # They should be added in PATH
+# <bitbar.title>RBC Stock/Currency tracker</bitbar.title>
+# <bitbar.author>schmooser</bitbar.author>
+# <bitbar.author.github>schmooser</bitbar.author.github>
+# <bitbar.image>https://cloud.githubusercontent.com/assets/480160/12206066/ee9db3be-b64f-11e5-96ae-11f7678cb905.png</bitbar.image>
+# <bitbar.desc>russian stock-market indicators from rbc.ru</bitbar.desc>
+# <bitbar.dependencies>jq,pup,curl</bitbar.dependencies>
 
 export PATH=/usr/local/bin:~/Applications/go/bin:$PATH
-json=$(curl -s -L http://rbc.ru | pup '.indicators__group json{}')
-out="`echo $json | jq -r '.[] | .children | .[1:] | .[] as $c | "\($c.children | map(.text) | join(" ")) | color=\(if $c.children | .[0] | .text == "▲" then "green" else "red" end) | href=\($c.href)"'`"
+json=$(curl -s -L http://rbc.ru | pup '.indicators__items json{}')
+# shellcheck disable=2016
+out=$(echo "$json" | jq -r '.children | .[] as $c | "\(if $c.children | .[2] | .class | contains("green") then "▲" else "▼" end) \($c.title) - \($c.children[1:] | map(.text) | join(" ")) | color=\(if $c.children | .[2] | .class | contains("green") then "green" else "red" end) | href=\($c.href)"')
 echo "$out" | grep Brent
 echo "---"
 echo "$out" | grep -v Brent | grep -v EUR/USD | grep USD | sed 's/color=green/color=111/;s/color=red/color=green/;s/color=111/color=red/'
