b6701f1cb7a601fc684186cbda53110c45ac96a2
cnsworder <cnsworder@gmail.com>
2016-04-10 15:21:08 +0800

    Add aria2c
    start aria2c and open aria2c  web ui

---
+++ Network/aria2.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+#
+# Quickly aria2c
+# The list of hosts are extracted from ~/.ssh/config
+#
+# <bitbar.title>aria2c</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>cnsworder</bitbar.author>
+# <bitbar.author.github>cnsworder</bitbar.author.github>
+# <bitbar.desc>Quickly aria2c</bitbar.desc>
+#
+
+echo "aria2c"
+echo "---"
+count=`ps aux | grep aria2c | grep -v grep | wc -l`
+if ((count < 1)); then
+    echo 'not running | bash=aria2c param1="-D --enable-rpc --rpc-allow-origin-all --rpc-listen-all -c -x 10 -s 10"'
+else
+    echo 'runing | color=green  href="http://ziahamza.github.io/webui-aria2/"'
+fi
