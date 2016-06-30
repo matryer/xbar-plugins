611b5c72acaae370bf860c8a536c04978cfbac54
Jonas Bach <hallo@jonasbach.de>
2016-06-30 19:01:53 +0200

    fix shellcheck errors

--- Finance/ginmon.1d.sh
+++ Finance/ginmon.1d.sh
@@ -9,9 +9,9 @@
 # <bitbar.desc>Shows daily performance of Ginmon funds. Add account via `$ security add-generic-password -a "<username>" -s ginmonbitbar -w "<password>"`.</bitbar.desc>
 
 # get user/pass from keychan
-SEC=`security find-generic-password -s ginmonbitbar -g 2>&1`
-USER=`echo "$SEC" | grep "acct" | cut -d \" -f 4`
-PASS=`echo "$SEC" | grep "password" | cut -d \" -f 2`
+SEC=$(security find-generic-password -s ginmonbitbar -g 2>&1)
+USER=$(echo "$SEC" | grep "acct" | cut -d \" -f 4)
+PASS=$(echo "$SEC" | grep "password" | cut -d \" -f 2)
 
 if [ -z "$USER" ] || [ -z "$PASS" ]; then
 	echo "Account details missing | color=red"
@@ -19,19 +19,19 @@ if [ -z "$USER" ] || [ -z "$PASS" ]; then
 fi
 
 # create temporary file
-TMP=`mktemp`
+TMP=$(mktemp)
 
 # login
 curl 'https://kundencenter.ginmon.de/login' --silent -L -A 'Mozilla/5.0' --cookie-jar "$TMP" >/dev/null
-HTML=`curl 'https://kundencenter.ginmon.de/login' --silent -L -A 'Mozilla/5.0' --data-urlencode "_username=$USER" --data-urlencode "&_password=$PASS" --cookie "$TMP" | tr -d '\040\011\012\015'`
+HTML=$(curl 'https://kundencenter.ginmon.de/login' --silent -L -A 'Mozilla/5.0' --data-urlencode "_username=$USER" --data-urlencode "&_password=$PASS" --cookie "$TMP" | tr -d '\040\011\012\015')
 
 # get number
-POSITIVE=`echo "$HTML" | grep -o 'valuepercentpositive">[[:digit:],]*' | cut -d '>' -f 2`
-NEGATIVE=`echo "$HTML" | grep -o 'valuepercentnegative">[[:digit:],]*' | cut -d '>' -f 2`
+POSITIVE=$(echo "$HTML" | grep -o 'valuepercentpositive">[[:digit:],]*' | cut -d '>' -f 2)
+NEGATIVE=$(echo "$HTML" | grep -o 'valuepercentnegative">[[:digit:],]*' | cut -d '>' -f 2)
 
 # output
 if [ ! -z "$POSITIVE" ]; then
-	SUM=`echo "$HTML" | grep -o 'valuecurrency-eurpositive">[[:digit:],]*' | cut -d '>' -f 2`
+	SUM=$(echo "$HTML" | grep -o 'valuecurrency-eurpositive">[[:digit:],]*' | cut -d '>' -f 2)
 	echo "+$POSITIVE% | color=green"
 	echo "---"
 	echo "+$SUM€ | color=green href=https://kundencenter.ginmon.de/uebersicht"
@@ -39,7 +39,7 @@ if [ ! -z "$POSITIVE" ]; then
 fi
 
 if [ ! -z "$NEGATIVE" ]; then
-	SUM=`echo "$HTML" | grep -o 'valuecurrency-eurnegative">[[:digit:],]*' | cut -d '>' -f 2`
+	SUM=$(echo "$HTML" | grep -o 'valuecurrency-eurnegative">[[:digit:],]*' | cut -d '>' -f 2)
 	echo "-$NEGATIVE% | color=red"
 	echo "---"
 	echo "-$SUM€ | color=red href=https://kundencenter.ginmon.de/uebersicht"
