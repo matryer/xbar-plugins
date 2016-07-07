70a7c5ccc502b2afed186d5acffdbe8c01dfb51f
Kevin Deldycke <kevin@deldycke.com>
2016-07-07 08:24:39 +0200

    Update description and dependencies.

--- Dev/Homebrew/package_manager.7h.py
+++ Dev/Homebrew/package_manager.7h.py
@@ -4,8 +4,8 @@
 # <bitbar.version>v1.1</bitbar.version>
 # <bitbar.author>Kevin Deldycke</bitbar.author>
 # <bitbar.author.github>kdeldycke</bitbar.author.github>
-# <bitbar.desc>List package updates available from Homebrew and Cask. Allows individual or full upgrades (if available).</bitbar.desc>
-# <bitbar.dependencies>python,homebrew,cask</bitbar.dependencies>
+# <bitbar.desc>List package updates available from Homebrew, Cask and Pip. Allows individual or full upgrades (if available).</bitbar.desc>
+# <bitbar.dependencies>python,homebrew,cask,pip</bitbar.dependencies>
 # <bitbar.image>https://i.imgur.com/oXL2Nyn.png</bitbar.image>
 # <bitbar.abouturl>https://github.com/kdeldycke/dotfiles/blob/master/dotfiles-osx/.bitbar/package_manager.7h.py</bitbar.abouturl>
 
