44ac9de51aa8fef617ba0dbc4321acaa7f7f6096
rcastley <robert.castley@gmail.com>
2016-07-09 11:22:20 +0100

    Removed Key/Secret

--- Tools/Catchpoint/catchpoint.m5.php
+++ Tools/Catchpoint/catchpoint.m5.php
@@ -1,18 +1,18 @@
 #!/usr/bin/php
 <?php
-# <bitbar.title>Catchpoint Alerts</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
-# <bitbar.author>Robert Castley</bitbar.author>
-# <bitbar.author.github>rcastley</bitbar.author.github>
-# <bitbar.desc>Display Catchpoint Alerts via the Pull API.</bitbar.desc>
-# <bitbar.image>http://i.imgur.com/xgkKWjy.png</bitbar.image>
-# <bitbar.dependencies>php, cURL</bitbar.dependencies>
-# <bitbar.abouturl>http://www.catchpoint.com/</bitbar.abouturl>
+// <bitbar.title>Catchpoint Alerts</bitbar.title>
+// <bitbar.version>v1.0</bitbar.version>
+// <bitbar.author>Robert Castley</bitbar.author>
+// <bitbar.author.github>rcastley</bitbar.author.github>
+// <bitbar.desc>Display Catchpoint Alerts via the Pull API.</bitbar.desc>
+// <bitbar.image>http://i.imgur.com/xgkKWjy.png</bitbar.image>
+// <bitbar.dependencies>php, cURL</bitbar.dependencies>
+// <bitbar.abouturl>http://www.catchpoint.com/</bitbar.abouturl>
 echo "| templateImage=iVBORw0KGgoAAAANSUhEUgAAABIAAAASCAYAAABWzo5XAAABrklEQVQ4jYXTy2oUURAG4G96ro7xmhARERHcZm3AhUJAxIW4Fl/B13Onj6EoatCgIJF4jZO59IyL+TvTMyFY0Jw6df3rr9OcLjdzPsDOiu2ENFfuLWxigkc4hz/oYR3beIMNDDA7rVAfj7GGV/EfYIQj/EijO3gb+4lCO7iI90F0kHNUO38l9jBot/ABipWxGkn6irMYx1fG18IQe9G7dURd3E/wx5zddC0xTfEynBS1SUpcx35RC94PR238TdIs3Ayjlxmxgd/mhN9A0cLlBA/yVd1m+F4jtBMOqwadNNjFrMCTFDhT46hCcryV6MPEjHPvZdRnjQTdDfttiw0N8NOyXEjDdpBPcQsvm7idgNJ8vRVvzRScpkjbfOWNjCWj9tFvBm5hvs5RrVAj0KvNnrd4AlXeeu5fCnyLYy3GifkbKpLYzyf+VvQr4XIPn6sH2Qy5G7hkvv5+DVEz3BQW76gTRJvSsZI+HuJ1RqvezjQoxhlxgmtB/SKxS/9aK2SOgqqRcY9ih6uxj2LbTaOlQpPMe4h7+JTEdgr34tvGc7yz2Oh/5Wk42Iq+Ssex/AOGoHohWJhtKAAAAABJRU5ErkJggg==\n";
 echo "---\n";
 
-$key    = 'RNM-gZ-jTUl6pV_I9';
-$secret = '6d37dc88-f9a3-4103-addd-ccba8c815fee';
+$key    = '<Catchpoint_Key>';
+$secret = '<Catchpoint_Secret>';
 
 $data = array('grant_type' => 'client_credentials', 'client_id' => $key, 'client_secret' => $secret);
 
@@ -21,7 +21,6 @@ $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://io.catchpoint.com/ui/api/token');
 curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($data));    
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-//curl_setopt($ch, CURLOPT_VERBOSE, true);
 
 $result = curl_exec($ch);
 
@@ -31,16 +30,11 @@ $result = json_decode($result);
 
 $_SESSION['token'] = base64_encode($result->access_token);
 
-//echo 'Token : ' . $_SESSION['token'] . "\r\n";
-
 $ch = curl_init();
-
-//echo "\r\nDisplay info for Node ID #174\r\n";
 
 curl_setopt($ch, CURLOPT_URL, 'https://io.catchpoint.com/ui/api/v1/alerts?pageSize=5');
 curl_setopt($ch, CURLOPT_HTTPHEADER, 
     array('Authorization: Bearer ' . $_SESSION['token']));
-//curl_setopt($ch, CURLOPT_VERBOSE, true);
 
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
 
