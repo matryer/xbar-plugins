ad62595072995cd9a4df93a18dae58e32f33cf56
ia <rrysteryfactory@gmail.com>
2016-07-25 12:13:54 -0400

    Plugin akalias (#486)
    * Was not printing all lines for me. Don't know why. This seems to fix it.
    
    * Disable shellcheck.

--- Dev/Shell/aka.1h.sh
+++ Dev/Shell/aka.1h.sh
@@ -9,32 +9,32 @@
 # <bitbar.dependencies></bitbar.dependencies>
 
 # MISSION STATEMENT
-# 
-# Creates a lookup for useful and timely commands (ie current-project-specific)
-# Can save a little typing and a lot of remembering. 
 #
-# - Prints each line your aka file. 
-# - Copies the command to your clipboard when clicked on. 
-# - Provides a link to edit the file. 
+# Creates a lookup for useful and timely commands (ie current-project-specific)
+# Can save a little typing and a lot of remembering.
+#
+# - Prints each line your aka file.
+# - Copies the command to your clipboard when clicked on.
+# - Provides a link to edit the file.
 
 
 # SET UP.
-#  
+#
 # Create a source aka.sh file of your own.
-# Make sure it's being sourced by .bash_profile, .bashrc, 
+# Make sure it's being sourced by .bash_profile, .bashrc,
 # or your own fancy .dotfiles config.
-# 
-# Then set the path in VARS below. That's it. 
+#
+# Then set the path in VARS below. That's it.
 
 # VARS
-# 
-# Set your own aka.sh/.zsh/.whatever path here. 
+#
+# Set your own aka.sh/.zsh/.whatever path here.
 aka_file=~/.dotfiles/bashers/aka.zsh
 
 
 # FUNCS
-# 
-# Click to open aka file in terminal with system's visual editor or vi as fallback. 
+#
+# Click to open aka file in terminal with system's visual editor or vi as fallback.
 if [[ "$1" = "editaka" ]]; then
 	# use system EDITOR or VISUAL or fallback default vi
 	# http://stackoverflow.com/questions/10725238/opening-default-text-editor-in-bash
@@ -50,28 +50,30 @@ fi
 
 
 # OUTPUTS
-# 
-# Header. 
+#
+# Header.
 echo "= | color=#1853C9"  # ∷ ⚯ ɐ ª
 echo '---'
 
-# Read each line in aka file. 
-while read -r line 
-do 
-	# Strip 'alias ' from line for legibility. 
+# Read each line in aka file.
+while read -r line
+do
+	# Strip 'alias ' from line for legibility.
 	dict="${line//alias /}"
 	# Find everything between " "
+	 #shellcheck disable=SC2116,SC2086
 	com="$(echo "$line" | grep -o '".*"' | sed 's/"//g')"
 
 	# Echo dict with com as single-quoted p2 argument.
 	# Clicking will copy just the command with pbcopy.
 	# echo "$(echo $dict) |bash=$0 param1=copy param2='$com' refresh=false terminal=false"
-	echo "$dict |bash=$0 param1=copy param2='$com' refresh=false terminal=false"
+    #shellcheck disable=SC2116,SC2086
+	echo "$(echo $dict) |bash=$0 param1=copy param2='$com' refresh=false terminal=false"
 
 done < "$aka_file"
 
 # OPTION TO EDIT AKA FILE
 echo ''
 echo "---"
-echo "Edit .aka file |bash=$0 param1=editaka refresh=true terminal=true" 
+echo "Edit .aka file |bash=$0 param1=editaka refresh=true terminal=true"
 
