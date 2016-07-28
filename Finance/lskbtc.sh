72d099ee9d4ae6a38d08d5ea78e40edfcff49546
Corv <mail@corv.in>
2016-07-28 23:35:38 +0700

    Fix linter error

--- Finance/lskbtc.sh
+++ Finance/lskbtc.sh
@@ -9,4 +9,4 @@
 # <bitbar.desc>Shows last Lisk price on Bitfinex in BTC.</bitbar.desc>
 # <bitbar.image>http://i.imgur.com/j5qaX2t.png</bitbar.image>
 
-echo -n "LSK "; curl -s https://poloniex.com/public\?command\=returnTicker | grep -Eo '"BTC_LSK":{"id":163,"last":"[0-9]+\.[0-9]+' | sed 's/"BTC_LSK":{"id":163,"last":"//'
+echo -n "LSK "; curl -s https://poloniex.com/public\?command=returnTicker | grep -Eo '"BTC_LSK":{"id":163,"last":"[0-9]+\.[0-9]+' | sed 's/"BTC_LSK":{"id":163,"last":"//'
