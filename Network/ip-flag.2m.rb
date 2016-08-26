e7827db96c970663371e6ba518038c914f085b82
Eric Ripa <eric@ripa.io>
2016-08-26 17:21:01 +0200

    Change from ipinfo.io to ip-api.com due to higher rate limit (#526)

--- Network/ip-flag.2m.rb
+++ Network/ip-flag.2m.rb
@@ -8,9 +8,11 @@
 # <bitbar.dependencies>OS X 10.11</bitbar.dependencies>
 
 require 'open-uri'
+require 'json'
 
 begin
-  country_code = open('http://ipinfo.io/country').string.chomp.split ''
+  cc = JSON.load(open('http://ip-api.com/json'))
+  country_code = cc['countryCode'].chomp.split ''
   c1, c2 = *country_code.map { |c| (c.ord + 0x65).chr.force_encoding 'UTF-8' }
   puts "\xF0\x9F\x87#{c1}\xF0\x9F\x87#{c2}"
 rescue StandardError => err
