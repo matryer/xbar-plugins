0deb4bd0d905af46098104ab4ee008f3528011a8
Richard Guay <raguay@customct.com>
2016-09-23 14:35:48 +0700

    Updated CurrentFiles.1h.rb plugin.

--- System/currentFiles.1h.rb
+++ System/currentFiles.1h.rb
@@ -4,15 +4,42 @@
 # <bitbar.title>Current Working Files</bitbar.title>
 # <bitbar.version>v0.1</bitbar.version>
 # <bitbar.author>Richard Guay</bitbar.author>
+# <bitbar.author.github>raguay</bitbar.author.github>
 # <bitbar.desc>List of files that I'm currently working on. It allows me to select which editor to use and integrates with Alfred workflow for BitBar.</bitbar.desc>
+# <bitbar.image>http://customct.com/images/CurrentFilesPlugin-01.png</bitbar.image>
+# <bitbar.dependencies>ruby</bitbar.dependencies>
+# <bitbar.abouturl>http://customct.com/bitbar</bitbar.abouturl>
+
+#
+# Global Variables:
+#
+checkbeg = ""
+checkin = ""
+vimProg = "/usr/local/Cellar/macvim/7.4-107/MacVim.app/Contents/MacOS/MacVim"
+sublimeProg = "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl"
+emacsProg = "/usr/local/bin/emacsclient"
+
+#
+# Check for needed files existing and create them
+# if they don't.
+#
+if ! File.exist?(Dir.home + "/.myCurrentFiles")
+  fh = File.new(Dir.home + "/.myCurrentFiles","w+")
+  fh.write("~/.zshrc\n~/.bashrc\n~/.zshenv\n~/.zlogin\n~/.profile\n~/.vimrc\n")
+  fh.close
+  fh = File.new(Dir.home + "/.myeditors","w+")
+  fh.write("Emacs|emacs\nMacVim|vim\nSublime Text|sublime\n")
+  fh.close
+  fh = File.new(Dir.home + "/.myeditorchoice","w+")
+  fh.write("sublime")
+  fh.close
+end
 
 #
 # Check for version of BitBar and set the check
 # to be the normal check or an emoji check depending
 # upon the version.
 #
-checkbeg = ""
-checkin = ""
 if ENV["BitBarVersion"] != nil
   #
   # The BitBar version is defined. It is at least version 2.0.0-beta10
@@ -28,53 +55,73 @@ else
 end
 
 if ARGV.empty?
+  #
+  # Create the menu.
+  #
   puts '🗃';
   puts "---";
   puts "Files To Edit:"
   cfn = File.expand_path(__FILE__)
   IO.readlines(Dir.home + "/.myCurrentFiles").each { |i|
-    fn = File.basename(i.chomp!)
-    puts "#{fn} | bash=\"#{cfn}\" param1=\"#{i}\" terminal=false"
+    fn = File.basename(i.chop!)
+    puts "#{fn} | bash=\"#{cfn}\" param1=\"file\" param2=\"#{i}\" terminal=false"
   }
   puts "---"
   puts "Editor to Use:"
   editor = IO.read(Dir.home + "/.myeditorchoice")
-  if editor == "emacs"
-    puts "#{checkbeg}Emacs Editor | bash=\"#{cfn}\" param1=\"emacs\" #{checkin} terminal=false refresh=true\n"
-  else
-    puts "Emacs Editor | bash=\"#{cfn}\" param1=\"emacs\" terminal=false refresh=true\n"
-  end
-  if editor == "sublime"
-    puts "#{checkbeg}Sublime Text Editor | bash=\"#{cfn}\" param1=\"sublime\" #{checkin} terminal=false refresh=true\n"
-  else
-    puts "Sublime Text Editor | bash=\"#{cfn}\" param1=\"sublime\" terminal=false refresh=true\n"
-  end
-  if editor == "vim"
-    puts "#{checkbeg}Vim Editor | bash=\"#{cfn}\" param1=\"vim\" terminal=false #{checkin} refresh=true\n"
-  else
-    puts "Vim Editor | bash=\"#{cfn}\" param1=\"vim\" terminal=false refresh=true\n"
-  end
+  IO.readlines(Dir.home + "/.myeditors").each { |i|
+    if i.chop! == ""
+        continue
+    end
+    parts = i.split("|")
+    parts[1]
+    if editor == parts[1]
+      puts "#{checkbeg}#{parts[0]} | bash=\"#{cfn}\" param1=\"#{parts[1]}\" #{checkin} terminal=false refresh=true\n"
+    else
+      puts "#{parts[0]} | bash=\"#{cfn}\" param1=\"#{parts[1]}\" terminal=false refresh=true\n"
+    end
+  }
 else
+  #
+  # Process a command.
+  #
   case ARGV[0]
-  when "emacs" then
-    IO.write(Dir.home + "/.myeditorchoice","emacs")
-  when "vim" then
-    IO.write(Dir.home + "/.myeditorchoice","vim")
-  when "sublime" then
-    IO.write(Dir.home + "/.myeditorchoice","sublime")
-  else
-    fn = ARGV[0]
+  when "file" then
+    #
+    # Open the file in the preferred editor.
+    #
+    fn = ARGV[1]
     if fn[0] == '~'
       fn = Dir.home + fn.slice(1,fn.length)
     end
     editor = IO.read(Dir.home + "/.myeditorchoice")
     case editor
     when "emacs" then
-      `/usr/local/bin/emacsclient -n "#{fn}"`
+      #
+      # Call the emacsclient program to open the file.
+      #
+      `#{emacsProg} -n "#{fn}"`
     when "vim" then
-      `'/usr/local/Cellar/macvim/7.4-106/MacVim.app/Contents/MacOS/MacVim' '#{fn}'`
+      #
+      # Call MacVim to open the file.
+      #
+      `'#{vimProg}' '#{fn}'`
     when "sublime" then
-      `'/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl' '#{fn}'`
+      #
+      # Call Sublime Text to open the file.
+      #
+      `'#{sublimeProg}' '#{fn}'`
+    else
+      #
+      # Else, let the system open the file with the
+      # desired program.
+      #
+      `/usr/bin/open -a '#{editor}' '#{fn}'`
     end
+  else
+    #
+    # Set the editor Choice.
+    #
+    IO.write(Dir.home + "/.myeditorchoice",ARGV[0])
   end
 end
