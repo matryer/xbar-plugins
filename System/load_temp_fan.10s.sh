4548c72d0a599c6bbc7184eb106b169a1e8a51c1
Ivan Kovnatsky <sevenfourk@gmail.com>
2016-10-10 22:55:53 +0300

    removed deps tag, added image tag

--- System/load_temp_fan.10s.sh
+++ System/load_temp_fan.10s.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
+
+#
 # <bitbar.title>Load Average/Temperature/Fan Speed</bitbar.title>
-# <bitbar.version>v1.9.2</bitbar.version>
+# <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Ivan Kovnatsky</bitbar.author>
 # <bitbar.author.github>sevenfourk</bitbar.author.github>
 # <bitbar.desc>This plugin displays the current CPU load, CPU temperature, Fan speed (requires iStats ruby gem)</bitbar.desc>
-# <bitbar.dependencies>iStats</bitbar.dependencies>
+# <bitbar.image>https://i.imgur.com/y3ytf1l.png</bitbar.image>
 # <bitbar.abouturl>https://github.com/matryer/bitbar-plugins/blob/master/System/load_temp_fan.10s.sh</bitbar.abouturl>
 #
 # iStats -- https://github.com/Chris911/iStats
 # gem install iStats
+#
 
 load=`uptime|sed 's/.*ages: //'`
 temp=`/usr/local/bin/istats cpu|awk '{print $3}'`
