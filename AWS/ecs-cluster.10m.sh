cb2f78c27348a8466b443eab53e15f58e7999cd5
Bob Zoller <bob@zoller.us>
2016-12-01 13:38:05 -1000

    fix shellcheck errors

--- AWS/ecs-cluster.10m.sh
+++ AWS/ecs-cluster.10m.sh
@@ -21,22 +21,22 @@ if [ -z "$ECS_CLUSTER" ]; then
 fi
 
 data=$( aws --profile $AWS_CLI_PROFILE ecs list-container-instances --output json --cluster $ECS_CLUSTER )
-instance_ids=$( echo $data | jq -r '.containerInstanceArns | map(. | split("/") | last) | join(" ")' )
+instance_ids=$( echo "$data" | jq -r '.containerInstanceArns | map(. | split("/") | last) | join(" ")' )
 
-data=$( aws --profile $AWS_CLI_PROFILE ecs describe-container-instances --output json --cluster $ECS_CLUSTER --container-instances $instance_ids )
-instance_count=$( echo $data | jq -r '.containerInstances | length' )
+data=$( aws --profile $AWS_CLI_PROFILE ecs describe-container-instances --output json --cluster $ECS_CLUSTER --container-instances "$instance_ids" )
+instance_count=$( echo "$data" | jq -r '.containerInstances | length' )
 
-total_memory=$(( $( echo $data | jq -r '.containerInstances | map(.registeredResources[] | select(.name == "MEMORY") | .integerValue) | add' ) / 1000 ))
-remaining_memory=$(( $( echo $data | jq -r '.containerInstances | map(.remainingResources[] | select(.name == "MEMORY") | .integerValue) | add' ) / 1000 ))
+total_memory=$(( $( echo "$data" | jq -r '.containerInstances | map(.registeredResources[] | select(.name == "MEMORY") | .integerValue) | add' ) / 1000 ))
+remaining_memory=$(( $( echo "$data" | jq -r '.containerInstances | map(.remainingResources[] | select(.name == "MEMORY") | .integerValue) | add' ) / 1000 ))
 reserved_memory=$(( total_memory - remaining_memory ))
 usage_memory=$( echo "$reserved_memory * 100 / $total_memory" | bc )
 
-total_cpu=$( echo $data | jq -r '.containerInstances | map(.registeredResources[] | select(.name == "CPU") | .integerValue) | add' )
-remaining_cpu=$( echo $data | jq -r '.containerInstances | map(.remainingResources[] | select(.name == "CPU") | .integerValue) | add' )
+total_cpu=$( echo "$data" | jq -r '.containerInstances | map(.registeredResources[] | select(.name == "CPU") | .integerValue) | add' )
+remaining_cpu=$( echo "$data" | jq -r '.containerInstances | map(.remainingResources[] | select(.name == "CPU") | .integerValue) | add' )
 reserved_cpu=$(( total_cpu - remaining_cpu ))
 usage_cpu=$( echo "$reserved_cpu * 100 / $total_cpu" | bc )
 
-function mono() {
+mono() {
   echo "$1 | font=Monaco trim=false"
 }
 
