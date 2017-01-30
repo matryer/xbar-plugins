a98961bffed153e7289fffd73c0cc8901a7fe7f9
Jan Groß <jan.gross@omg.de>
2017-01-30 12:07:16 +0100

    Fixed: "Report an issue" link Added: Refresh on lunch start Submenu version bump

--- Time/shiftstats.30s.py
+++ Time/shiftstats.30s.py
@@ -76,7 +76,7 @@ def get_lunchbreak_time():
 
 def start_lunch_button():
     #Call this script with the --start-lunch flag
-    print "-- Start Lunch break | color=indianred bash="+__file__+ " param1='--start-lunch' terminal='false' color=green"
+    print "-- Start Lunch break | refresh=true | color=indianred bash="+__file__+ " param1='--start-lunch' terminal='false' color=green"
 
 def print_lunch_info():
     last_lunch = get_lunchbreak_time()[:19]  #Has to be cut after 19 characters because unconverted data remains otherwise (probably the newline)
@@ -135,9 +135,9 @@ def print_sub():
     print "About this plugin"
     print "-- Developed by Jan M. Groß | color=black"
     print "-- Images/Icons from icons8.com | href=http://icons8.com"
-    print "-- Version 1.0"
+    print "-- Version 2.0"
     print "-- ---"
-    print "-- Report an Issue | href=" # Note: url for issue reports once its published => https://github.com/matryer/bitbar-plugins/issues/new?assignee=jangross&title=re:%20Workshift%20Stats&body=FAO:+(@jangross)
+    print "-- Report an Issue | href=https://github.com/matryer/bitbar-plugins/issues/new?assignee=jangross&title=re:%20Workshift%20Stats&body=FAO:+(@jangross)"
 
 print_main()
 print_sub()
