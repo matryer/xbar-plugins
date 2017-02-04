0666ea207ac966d45558e68329a33a6f64a575ad
Ernst Widerberg <ernstwi@kth.se>
2017-02-04 23:28:40 +0100

    Taskpaper Today 1.1 (#686)
    * Add Taskpaper Today plugin
    
    * Ignore case for "Archive" header
    
    * Allow "@today" tags to appear anywhere in line
    
    * Add comments
    
    * Edit description
    
    * Increment version number
    
    * Remove blank line

--- Lifestyle/taskpaper.10s.py
+++ Lifestyle/taskpaper.10s.py
@@ -2,18 +2,18 @@
 
 #
 # <bitbar.title>Taskpaper Today</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.version>v1.1</bitbar.version>
 # <bitbar.author>Ernst Widerberg</bitbar.author>
 # <bitbar.author.github>ernstwi</bitbar.author.github>
-# <bitbar.desc>Display Taskpaper items tagged "today" in the menu bar.</bitbar.desc>
+# <bitbar.desc>This plugin will display all tasks tagged @today and not @done in a given Taskpaper file.</bitbar.desc>
 # <bitbar.image>http://i.imgur.com/XODr2PY.png</bitbar.image>
 # <bitbar.dependencies>python</bitbar.dependencies>
-#
 #
 # Set you own file location by changing the variable "taskpaper_file" below.
 #
 
 import os
+import re
 import sys
 
 taskpaper_file = '~/todo.taskpaper'
@@ -32,12 +32,19 @@ header = ''
 items = ''
 num_items = 0
 for line in file:
-    if line == 'archive:\n':
+
+    # Stop searching if "Archive" header is reached.
+    if re.match(r'archive:\n', line, re.IGNORECASE) != None:
         break
+
+    # Update header (project title).
     if line.endswith(':\n'):
         header = line.strip()[:-1]
-    elif line.endswith('@%s\n' % tag):
-        items += '%s(%s)\n' % (line.partition('@' + tag)[0][2:], header)
+
+    # Include lines that contain @today and not @done in output. Remove leading
+    # dash and "@today" with surrounding whitespace.
+    elif ' @%s' % tag in line and ' @done' not in line:
+        items += '%s (%s)\n' % (re.sub(' @%s ?' % tag, ' ', line).strip()[2:], header)
         num_items += 1
 
 file.close()
