3a7d1313463e4d0857ae320f5a4e37e874e08129
drselump14 <botoksgonzales@gmail.com>
2017-02-05 13:24:39 +0900

    add notmuch email counter

---
+++ Email/notmuch-mail-count.30s.rb
@@ -0,0 +1,27 @@
+#!/usr/bin/env ruby
+# encoding: UTF-8
+
+# <bitbar.title>Change mutt inbox</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Slamet Kristanto</bitbar.author>
+# <bitbar.author.github>drselump14</bitbar.author.github>
+# <bitbar.desc>Show muttbar inbox using notmuch inbox and unread</bitbar.desc>
+# <bitbar.dependencies>mutt,notmuch</bitbar.dependencies>
+
+inbox = `/usr/local/bin/notmuch search tag:inbox tag:unread`
+
+mails = inbox.split("\n")
+count = mails.length
+if count > 0
+  puts "📮 #{count} | color=red size=12"
+else
+  puts ''
+end
+
+puts "---"
+
+mails.each do |m|
+  mail = m.gsub(/thread.*\]/,'')[0..25].gsub(/\|/,':')
+  puts "#{mail} | color=green size=12 bash='/usr/local/bin/mutt' "
+end
+
