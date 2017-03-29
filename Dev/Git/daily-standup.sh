5dd8f8b5f7a2112c4a64b3b951d5ea807867c641
krishna <krish.kumar@gmail.com>
2017-03-29 18:56:24 +0530

    Fixes for Travis CI issues

--- Dev/Git/daily-standup.sh
+++ Dev/Git/daily-standup.sh
@@ -21,68 +21,68 @@ BRONZEMEDAL=🥉 # Atleast 1 commit today
 PROJECTINDICATOR=📂
 EMPTY_INDICATOR=❄️
 ALERT_INDICATOR=🚨
-ALERTMESSAGE="Earn medals... Get to work! | color=blue"
+DAYS_SINCE_COMMIT_MESSAGE="Earn medals... Get to work! | color=blue"
 DAYS_SINCE_COMMIT_CONTENT=" days since last commit "
-user=`whoami`
-PROJECTS_DIRECTORY=/Users/krishna/projects/
+user=$(whoami)
+PROJECTS_DIRECTORY="/Users/$user/projects/"
 DAYS_SINCE_COMMIT=0
 
 # Start
-COUNT=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup | grep "<$user>" | wc -l`
+COUNT=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup | grep -c "<$user>")
 COUNT="$COUNT"
-if [ $COUNT = 0 ]
+if [ "$COUNT" = 0 ]
 then
     COUNT=""
     COMMITINDICATOR=$EMPTY_INDICATOR
     DAYS_SINCE_COMMIT=1
-    COUNT2=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 2 | grep "<$user>" | wc -l`
-    COUNT3=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 3 | grep "<$user>" | wc -l`
-    COUNT4=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 4 | grep "<$user>" | wc -l`
-    COUNT5=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 5 | grep "<$user>" | wc -l`
-    COUNT6=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 6 | grep "<$user>" | wc -l`
-    COUNT7=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 7 | grep "<$user>" | wc -l`
-    COUNT8=`cd $PROJECTS_DIRECTORY;/usr/local/bin/git-standup -d 8 | grep "<$user>" | wc -l`
-    if [ $COUNT2 = 0 ]
+    COUNT2=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 2 | grep -c "<$user>")
+    COUNT3=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 3 | grep -c "<$user>") 
+    COUNT4=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 4 | grep -c "<$user>") 
+    COUNT5=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 5 | grep -c "<$user>") 
+    COUNT6=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 6 | grep -c "<$user>") 
+    COUNT7=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 7 | grep -c "<$user>") 
+    COUNT8=$(cd "$PROJECTS_DIRECTORY" || exit ;/usr/local/bin/git-standup -d 8 | grep -c "<$user>") 
+    if [ "$COUNT2" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=2
     fi
-    if [ $COUNT3 = 0 ]
+    if [ "$COUNT3" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=3
     fi
-    if [ $COUNT4 = 0 ]
+    if [ "$COUNT4" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=4
     fi
-    if [ $COUNT5 = 0 ]
+    if [ "$COUNT5" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=5
     fi
-    if [ $COUNT6 = 0 ]
+    if [ "$COUNT6" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=6
     fi
-    if [ $COUNT7 = 0 ]
+    if [ "$COUNT7" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR$EMPTY_INDICATOR
       DAYS_SINCE_COMMIT=7
     fi
-    if [ $COUNT8 = 0 ]
+    if [ "$COUNT8" = 0 ]
     then
       COUNT=""
       COMMITINDICATOR=$ALERT_INDICATOR
-      DAYS_SINCE_COMMIT=8
+      DAYS_SINCE_COMMIT=7+
     fi
 fi
 if [[ $COUNT -gt 0 ]]
@@ -105,12 +105,12 @@ echo $COMMITINDICATOR
 echo ---
 
 # On Click
-cd $PROJECTS_DIRECTORY
+cd "$PROJECTS_DIRECTORY" || exit 
 STANDUP=$(/usr/local/bin/git-standup)
 if [[ -z "$STANDUP" ]]; then
   echo "$DAYS_SINCE_COMMIT| size=30 "
   echo "$DAYS_SINCE_COMMIT_CONTENT"
-  echo "$ALERTMESSAGE"
+  echo "$DAYS_SINCE_COMMIT_MESSAGE"
 elif [[ -n "$STANDUP" ]]; then
   /usr/local/bin/git-standup | sed "s@$PROJECTS_DIRECTORY@$PROJECTINDICATOR@;n"
 fi
