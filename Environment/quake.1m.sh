f547124e6160fd31f582666dacfc54ae578f2ce9
thetall0ne <aaronedell@gmail.com>
2017-04-05 14:38:51 -0700

    Added https and changed v to 1.0
    For some reason this stopped working. Adding http’s’ and changing
    version to 1.0 fixed it

--- Environment/quake.1m.sh
+++ Environment/quake.1m.sh
@@ -4,11 +4,18 @@
 # <bitbar.author>Aaron Edell</bitbar.author>
 # <bitbar.author.github>aaronedell</bitbar.author.github>
 # <bitbar.desc>Displays the most significant earthquakes in the last 30 days from http://earthquake.usgs.gov </bitbar.desc>
-# <bitbar.image>http://i.imgur.com/lF8Qdpk.png</bitbar.image>
-# <bitbar.dependencies>bash</bitbar.dependencies>
+# <bitbar.image>http://imgur.com/lF8Qdpk</bitbar.image>
+# <bitbar.dependencies>Bash GNU AWK</bitbar.dependencies>
 # <bitbar.abouturl>http://earthquake.usgs.gov</bitbar.abouturl>
+
+
+
 echo '🌎'
 echo '---'
-echo ; curl -s http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv | tail -n +2 | awk -F"," '{print $5" " $14$15}'
+
+echo ; curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv | tail -n +2 | awk -F"," '{print $5" " $14$15}'
 echo '---'
 echo 'Data from http://earthquake.usgs.gov | color=green href=http://earthquake.usgs.gov'
+
+
+
