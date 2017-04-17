604e673b6f5b01500d947a6c96f1c2ad8aad3bec
AaronEdell <aaronedell@gmail.com>
2017-04-17 13:48:45 -0700

    Fixed https (#738)
    * shortened city names
    
    shortened city names to take up less space
    
    * Added https and changed v to 1.0
    
    For some reason this stopped working. Adding http’s’ and changing
    version to 1.0 fixed it
    
    * removed timezoneplus file
    
    How did it get in there? I don’t know. I deleted it from by GitHub
    directory locally, hopefully this works.
    
    * Changed the image link
    
    Changed it to http://i.imgur.com/lF8Qdpk.png
    
    Not sure how it got changed in the first place

--- Environment/quake.1m.sh
+++ Environment/quake.1m.sh
@@ -5,10 +5,17 @@
 # <bitbar.author.github>aaronedell</bitbar.author.github>
 # <bitbar.desc>Displays the most significant earthquakes in the last 30 days from http://earthquake.usgs.gov </bitbar.desc>
 # <bitbar.image>http://i.imgur.com/lF8Qdpk.png</bitbar.image>
-# <bitbar.dependencies>bash</bitbar.dependencies>
+# <bitbar.dependencies>Bash GNU AWK</bitbar.dependencies>
 # <bitbar.abouturl>http://earthquake.usgs.gov</bitbar.abouturl>
+
+
+
 echo '🌎'
 echo '---'
-echo ; curl -s http://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv | tail -n +2 | awk -F"," '{print $5" " $14$15}'
+
+echo ; curl -s https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.csv | tail -n +2 | awk -F"," '{print $5" " $14$15}'
 echo '---'
 echo 'Data from http://earthquake.usgs.gov | color=green href=http://earthquake.usgs.gov'
+
+
+
