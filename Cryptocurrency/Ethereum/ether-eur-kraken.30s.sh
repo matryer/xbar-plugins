9944c066dc2a54210dde0c93192f498c2add7957
Guillaume Grelet <ggrelet@PRO.local>
2017-05-25 22:18:45 +0200

    Adding ether tu euro rate from kraken

---
+++ Cryptocurrency/Ethereum/ether-eur-kraken.30s.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# <bitbar.title>Kraken.com ETHEUR last price</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>G.G.</bitbar.author>
+# <bitbar.author.github>ggrelet</bitbar.author.github>
+# <bitbar.desc>Gives the last price of ether to euro from Kraken.com</bitbar.desc>
+# <bitbar.image>http://www.hosted-somewhere/pluginimage</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+# <bitbar.abouturl>http://url-to-about.com/</bitbar.abouturl>
+
+val=$(curl -s "https://api.kraken.com/0/public/Ticker?pair=ETHEUR" | tr -d '{}"[]' | tr ':,' '\n' | grep -A1 "^c$" | tail -1)
+
+echo "`printf "Ξ %.3f \n" "$val"` | size=13"
+echo "---"
+echo "Kraken.com | href="https://www.kraken.com/""
