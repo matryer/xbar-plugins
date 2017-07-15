955f2aac3d620f632a2f829c7f48b075c8248ec6
Timothy Barnard <test>
2017-07-15 18:33:22 +0100

    removed Files folder and implemented the bash script to create if needed
    updated image url path
    
    fixed some syntax issues

--- Dev/Gitty/Scripts/find_git.sh
+++ Dev/Gitty/Scripts/find_git.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-if [[ "$1" = "copy" ]]; then
+if ["$1" = "copy"] then
     echo "$2" | pbcopy
 fi
 
-if [[ "$1" = "run" ]]; then
+if ["$1" = "run"] then
     V_MY_PATH=$HOME
-    find "$V_MY_PATH/Documents" -name .git -type d -prune -not -path '*/.*/*' > "$V_MY_PATH/Documents/MacBarPlugins/Files/repos.txt"
+    find "$V_MY_PATH/Documents" -name .git -type d -prune -not -path '*/.*/*' > "$V_MY_PATH/Documents/MacBarPlugins/repos.txt"
 fi
 
 
