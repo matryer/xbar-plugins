22b2066af222ae77714394c9e2701829217edf09
Bryan Stone <aegixx@gmail.com>
2017-08-07 15:06:27 -0500

    Move to v2 API
    Signed-off-by: Bryan Stone <aegixx@gmail.com>

--- Cryptocurrency/Bitcoin/coinbase.com/balance.1h.sh
+++ Cryptocurrency/Bitcoin/coinbase.com/balance.1h.sh
@@ -1,8 +1,24 @@
 #!/bin/bash
 # <bitbar.title>Coinbase.com Your Balance</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.version>v1.1</bitbar.version>
 # <bitbar.author>Mat Ryer</bitbar.author>
 # <bitbar.author.github>matryer</bitbar.author.github>
-# <bitbar.desc>Shows your balance in BTC.  Be sure you add you API key.</bitbar.desc>
+# <bitbar.desc>Shows your balance in BTC.  Be sure you add your API & account details.</bitbar.desc>
 
-echo -n "BTC: "; curl -s "https://coinbase.com/api/v1/account/balance?api_key=YOUR_API_KEY" | egrep -o '"amount":"[0-9]+(\.)?([0-9]+").*?' | sed 's/"amount"://' | sed 's:^.\(.*\).$:\1:'
+API_KEY="YOUR_API_KEY"
+API_SECRET="YOUR_API_SECRET"
+ACCOUNT_ID="YOUR_ACCOUNT_ID"
+API_VERSION="2017-08-07" # You'll need to verify this in your Account Details
+
+TIMESTAMP=$(date +%s)
+REQUEST_METHOD="GET"
+REQUEST_URL="https://api.coinbase.com"
+REQUEST_PATH="/v2/accounts/${ACCOUNT_ID}"
+SIGNATURE=$(echo -n "${TIMESTAMP}${REQUEST_METHOD}${REQUEST_PATH}" | openssl dgst -sha256 -hmac "${API_SECRET}")
+
+echo -n "BTC: $(curl -s ${REQUEST_URL}${REQUEST_PATH} \
+  --request ${REQUEST_METHOD} \
+  --header "CB-VERSION: ${API_VERSION}" \
+  --header "CB-ACCESS-KEY: ${API_KEY}" \
+  --header "CB-ACCESS-SIGN: ${SIGNATURE}" \
+  --header "CB-ACCESS-TIMESTAMP: ${TIMESTAMP}" | jq -r ".data.native_balance.amount")"
