6063995eac40b4c8d8e26afb2c93f18a44bdfeeb
Paul Rankin <hello@paulwrankin.com>
2017-10-04 14:15:49 +1000

    New plugin: CPU Load (#867)
    * New plugin: CPU Load
    
    * Use hw.logicalcpu
    
    This will only count cores available in current power mode, and hw.ncpu appears
    unavailable for some Macs
    
    * Fix incorrect title

---
+++ System/cpu-load.5s.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+# <bitbar.title>CPU Load</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Paul W. Rankin</bitbar.author>
+# <bitbar.author.github>rnkn</bitbar.author.github>
+# <bitbar.desc>Shows CPU load as a percentage (without using top).</bitbar.desc>
+# <bitbar.image>http://photos.paulwrankin.com/screenshots/cpu-load.png</bitbar.image>
+# <bitbar.dependencies>bash</bitbar.dependencies>
+
+# BitBar CPU Load plugin
+
+ncpu=$(sysctl -n hw.logicalcpu)
+total=$(ps -Ao %cpu= | paste -sd+ - | bc)
+usage=$(echo "scale = 2; $total / $ncpu" | bc)
+
+printf "%0.1f%%\n" "$usage"
+echo "---"
+echo "Refresh | refresh=true"
