95fcf68eee2a8f8c4dd93d355e50ef1301cf2f4e
Chandler Davidson <chandlermdavidson@gmail.com>
2017-10-12 15:15:45 -0500

    Fixed Semicolons

--- Weather/emojiWeather.30m.js
+++ Weather/emojiWeather.30m.js
@@ -15,7 +15,7 @@ let request = require('request');
 
 let apiKey = '';    // https://home.openweathermap.org/api_keys
 let city = 'Huntsville';
-let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
+let url = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`;
 
 let emojis = [
     ':cry:',        // No connection
@@ -27,14 +27,14 @@ let emojis = [
     ':snowflake:',  // Snow
     '::',           // 
     ':sunny:',      // Clear
-    ':cyclone:']    // Extreme
+    ':cyclone:'];    // Extreme
 
 request(url, function (err, response, body) {   // Collect JSON object
     if (err) {  // If no connection, so sad...
         console.log(emojis[0]);
     } else {
 
-        let weather = JSON.parse(body)
+        let weather = JSON.parse(body);
         let id = weather.weather[0].id; // Seperate the weather ID
         id = Math.ceil(id / 100);       // Get only the hundreds place (1xx, 2xx)
 
