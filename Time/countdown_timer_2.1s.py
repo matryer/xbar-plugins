4186d4e2dba68025d00068321a346b2eb61b741e
Federico Ferri <federico.ferri.it@gmail.com>
2017-11-13 21:36:24 +0100

    use screenshot from github repo

--- Time/countdown_timer_2.1s.py
+++ Time/countdown_timer_2.1s.py
@@ -6,7 +6,7 @@
 # <bitbar.author.github>fferri</bitbar.author.github>
 # <bitbar.desc>Simple countdown timer.</bitbar.desc>
 # <bitbar.dependencies>python</bitbar.dependencies>
-# <bitbar.image>https://lh6.googleusercontent.com/IZAQc1rxln4Ye1KUlsaKzm_inGb-ligulk1Artxoa09gzqaoTpp1QS1Yj61lr-VTTcW_GTsMlC8EaQ=w2482-h1718</bitbar.image>
+# <bitbar.image>https://raw.githubusercontent.com/fferri/bitbar-countdown-timer/master/screenshot.gif</bitbar.image>
 # <bitbar.abouturl>https://github.com/fferri/bitbar-countdown-timer</bitbar.abouturl>
 
 import os
