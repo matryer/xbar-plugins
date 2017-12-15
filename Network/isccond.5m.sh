b71ebe5967ded32367715038dbaaada40a71c8b0
RT Hatfield <arthur_hatfield@homedepot.com>
2017-12-15 13:00:03 -0500

    Added ISC condition monitor

---
+++ Network/isccond.5m.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# <bitbar.title>ISC Condition Monitor</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author></bitbar.author>
+# <bitbar.author.github>panzertime</bitbar.author.github>
+# <bitbar.desc>Gets ISC's condition code and displays it</bitbar.desc>
+# <bitbar.image></bitbar.image>
+# <bitbar.dependencies></bitbar.dependencies>
+# <bitbar.abouturl>https://github.com/panzertime/bitbar-plugins</bitbar.abouturl>
+
+color=$(curl --silent -m 1 https://isc.sans.edu/infocon.txt | tr [a-z] [A-Z])
+echo "ISC Cond ${color} | color=${color}"
