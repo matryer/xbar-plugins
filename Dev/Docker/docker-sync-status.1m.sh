2ef513216a0fc25278c3056701fe18804d3ba51b
Brendan Shanny <brendan.shanny@securedocs.com>
2018-02-28 23:44:05 -0800

    Fix build errors

--- Dev/Docker/docker-sync-status.1m.sh
+++ Dev/Docker/docker-sync-status.1m.sh
@@ -5,7 +5,7 @@
 # <bitbar.author>Brendan Shanny</bitbar.author>
 # <bitbar.author.github>brenshanny</bitbar.author.github>
 # <bitbar.desc>Display the current status of docker-sync.</bitbar.desc>
-# <bitbar.image>https://imgur.com/a/vpCUK</bitbar.image>
+# <bitbar.image>https://i.imgur.com/LbaqXzU.png</bitbar.image>
 # <bitbar.dependencies>docker-sync</bitbar.dependencies>
 #
 # Docker-Sync Status Plugin
@@ -28,7 +28,7 @@ docker_log="/path/to/your/project/.docker-sync/daemon.log"
 # The path to a text file within your project that docker-sync will watch
 docker_checker="/path/to/your/project/docker-sync-checker.txt"
 
-first_file_access=`stat -f '%m' $docker_log`
+first_file_access=$(stat -f '%m' $docker_log)
 
 # Trigger a sync of the file
 echo "test docker-sync" >> $docker_checker
@@ -36,7 +36,7 @@ echo "test docker-sync" >> $docker_checker
 # 3s works just fine, 5s provides a safety margin
 sleep 5
 
-second_file_access=`stat -f '%m' $docker_log`
+second_file_access=$(stat -f '%m' $docker_log)
 
 # Reset the file
 echo "This file is used by the bitbar docker-sync-status plugin to check the status of docker-sync." > $docker_checker
