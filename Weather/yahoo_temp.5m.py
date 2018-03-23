4f3c9ee1abef2ebc451a50f7501161b69b9ecc69
mgjo5899 <mgjo5899@gmail.com>
2018-03-23 00:46:53 -0500

    Yahoo weather plugin version 2

--- Weather/yahoo_temp.5m.py
+++ Weather/yahoo_temp.5m.py
@@ -2,40 +2,59 @@
 # coding=utf-8
 #
 # <bitbar.title>Yahoo Weather</bitbar.title>
-# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.version>v2.0</bitbar.version>
 # <bitbar.author>mgjo5899</bitbar.author>
 # <bitbar.author.github>mgjo5899</bitbar.author.github>
-# <bitbar.desc>It tells you current condition of your chosen city.  Very simple app with simple configuration.</bitbar.desc>
+# <bitbar.desc>It tells you the current weather condition of the location where your computer is located at.  It knows the location of the computer by using its public IP.  You can also manually set the city and region through modifying the file. </bitbar.desc>
 # <bitbar.image>https://i.imgur.com/YNypf0P.jpg</bitbar.image>
 # <bitbar.dependencies>python</bitbar.dependencies>
 #
 # by mgjo5899
 
-import subprocess
+import urllib
 import json
 
-# 1. Go to Yahoo's developer website for weather
-#     - https://developer.yahoo.com/weather/#curl
-# 2. Click the Forecast for Nome, AK example
-# 3. Change the end part of YQL Query to your chosen city, state
-#     - For Champaign, IL: select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="champaign, il")
-#     - For Seoul, South Korea: select * from weather.forecast where woeid in (select woeid from geo.places(1) where text="seoul, kr")
-# 4. Copy the Endpoint from the website, below the response, and paste it here 
-url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22champaign%2C%20il%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+ip_url = 'http://ip-api.com/json'
 
-r = subprocess.check_output(['curl', url], stderr=subprocess.STDOUT)
-s = r.find('query') - 2
-j = json.loads(r[s:])
+try:
+  r = urllib.urlopen(ip_url).read()
+except IOError:
+  print("Server loading...")
+  exit(1)
 
-# Change unit to 'C' if you want Celsius
-# Change unit to 'F' if you want Fahrenheit
-unit = 'C'
+j = json.loads(r)
+city = str(j['city'])
+region = str(j['region'])
 
-cond = j['query']['results']['channel']['item']['condition']['text']
-temp = float(j['query']['results']['channel']['item']['condition']['temp'])
+####### IF YOU WANT TO MANUALLY CHOOSE THE LOCATION #########
+#############################################################
+# 1. Set the city and region as needed
+#city = 'quebec'
+#region = 'canada'
 
-if unit == 'C':
+url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22' + city + '%2C%20' + region + '%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys'
+
+try:
+  r = urllib.urlopen(url).read()
+except IOError:
+  print("Server loading...")
+  exit(1)
+
+j = json.loads(r)
+
+# change unit to 'c' if you want celsius
+# change unit to 'f' if you want fahrenheit
+unit = 'c'
+
+try:
+  cond = j['query']['results']['channel']['item']['condition']['text']
+  temp = float(j['query']['results']['channel']['item']['condition']['temp'])
+except TypeError:
+  print("Server Loading...")
+  exit(1)
+
+if unit == 'c':
   temp = (temp  - 32.0) * 5.0 / 9.0
   print(str(cond) + ' : ' + str(int(temp)) + '°C')
-elif unit == 'F':
+elif unit == 'f':
   print(str(cond) + ' : ' + str(int(temp)) + '°F')
