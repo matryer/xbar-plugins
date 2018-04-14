cffad037d0b908613b940e6fd1d2259fac6f0f41
Robin Moser <robin_moser@ymail.com>
2018-04-14 13:05:13 +0200

    Add Option to show/hide Desktop Icons

--- System/showhide-files.1d.py
+++ System/showhide-files.1d.py
@@ -17,7 +17,15 @@ if len(sys.argv) == 1:
     print '---'
     print 'show hidden files | terminal=false bash=%s param1=show' % sys.argv[0]
     print 'hide hidden files | terminal=false bash=%s param1=hide' % sys.argv[0]
+    print '---'
+    print 'show desktop icons | terminal=false bash=%s param1=showicn' % sys.argv[0]
+    print 'hide desktop icons | terminal=false bash=%s param1=hideicn' % sys.argv[0]
 elif sys.argv[1] == 'show':
     os.system('defaults write com.apple.finder AppleShowAllFiles YES && killall Finder')
 elif sys.argv[1] == 'hide':
     os.system('defaults write com.apple.finder AppleShowAllFiles NO && killall Finder')
+elif sys.argv[1] == 'showicn':
+    os.system('defaults write com.apple.finder CreateDesktop YES && killall Finder')
+elif sys.argv[1] == 'hideicn':
+    os.system('defaults write com.apple.finder CreateDesktop NO && killall Finder')
+
