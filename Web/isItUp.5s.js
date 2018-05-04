f7c22e74b9b0774b74f217ddeacd7a2aae617ec6
ZacharyDavidSaunders <zacharydavidsaunders@gmail.com>
2018-05-04 12:51:35 -0700

    Updated to v1.1

--- Web/isItUp.5s.js
+++ Web/isItUp.5s.js
@@ -1,28 +1,28 @@
 #!/usr/bin/env /usr/local/bin/node
 
 // <bitbar.title>Is It Up?</bitbar.title>
-// <bitbar.version>v1.0</bitbar.version>
+// <bitbar.version>v1.1</bitbar.version>
 // <bitbar.author>Zachary David Saunders</bitbar.author>
 // <bitbar.author.github>ZacharyDavidSaunders</bitbar.author.github>
 // <bitbar.desc>A MacOs Menu Bar (BitBar) plugin that allows you to check whether or not a website is currently up (online).</bitbar.desc>
 // <bitbar.dependencies>node.js</bitbar.dependencies>
 // <bitbar.image>https://i.imgur.com/BxHFJMn.png</bitbar.image>
-
-
 /*jshint esversion: 6 */
 
 
 
 //👋👋👋 Feel free to change the following varaibles to best suit your needs.
-var upMessage = '✅(Yes)';
-var unknownMessage = '⚠️(Error, click for details)';
-var downMessage = '🔥(No)';
+var upMessage = 'Yes ✅';
+var unknownMessage = 'Error, click for details ⚠️';
+var downMessage = 'No 🔥';
 var website = "www.google.com";
+
+
+
 //<--- DO NOT EDIT THE CODE BELOW THIS LINE. --->
-
-
-
 const https = require('https');
+
+var version = "v1.1";
 
 renderPlugin();
 
@@ -31,22 +31,23 @@ function renderPlugin(){
     var menuBarIcon = "Is \""+website+"\" up?: " + getMessage(status);
     if(status === "up"){
       console.log(menuBarIcon+" |color:green");
+      console.log("---");
+      console.log("Visit \""+website+"\". [Click to visit site] |href=https://"+website);
     }else if (status === "down"){
       console.log(menuBarIcon+" |color:red");
     }else{
       console.log(menuBarIcon+" |color:yellow");
-    }
-    if(getMessage(status) === unknownMessage){
       console.log("---");
-      console.log("ERROR: Unable to contact \""+ website+"\".");
-      console.log("Please verify that you entered the websute correctly (remember, you don't need a www/http/https prefix).");
-      console.log("If this problem persists, isitdown.site's API may be unavailable. ☹️");
+      console.log("Error: Unable to contact \""+ website+"\".");
+      console.log("Please verify that you entered the website correctly (remember, you don't need a www/http/https prefix).");
+      console.log("If this problem persists, isitdown.site's API may be unavailable.");
     }
+
     console.log("---");
-    console.log("Powered by \"isitdown.site\". [Click to visit site] |href=https://isitdown.site color:green");
-    console.log("For more information please see the github repository. [Click to visit site] |href=https://github.com/ZacharyDavidSaunders/IsItUp-BitBarPlugin color:green");
+    console.log("Powered by \"isitdown.site\". [Click to visit site] |href=https://isitdown.site");
+    console.log("For more information, please see the github repository. [Click to visit site] |href=https://github.com/ZacharyDavidSaunders/IsItUp-BitBarPlugin");
     console.log("---");
-    console.log("(© Zachary David Saunders 2018) | size:10");
+    console.log("Version: "+version+"\n© Zachary David Saunders 2018 |size:10");
   });
 }
 
@@ -63,18 +64,15 @@ function checkWebsite(callback){
         var response = JSON.parse(data);
         if(response.isitdown == false){
           result = "up";
-          callback(result);
         }else if (response.isitdown == true){
           result = "down";
-          callback(result);
         }else{
           result = "unknown";
-          callback(result);
         }
       }catch(error){
         result = "unknown";
-        callback(result);
       }
+      callback(result);
     });
 
   }).on("error", (err) => {
