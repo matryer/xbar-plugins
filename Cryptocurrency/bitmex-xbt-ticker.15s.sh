edc1ca3739c8c2d94e9ea98bfddb9f0d6e4235ba
horimislime <horimislime@gmail.com>
2018-05-13 00:07:16 +0900

    Fix syntax

--- Cryptocurrency/bitmex-xbt-ticker.15s.sh
+++ Cryptocurrency/bitmex-xbt-ticker.15s.sh
@@ -9,26 +9,26 @@
 # <bitbar.dependencies>bash<bitbar.dependencies>
 
 API_URL='https://www.bitmex.com/api/v1/quote?symbol=XBT&count=1&reverse=true'
-JSON=$(curl -s $API_URL)
+JSON=$(curl -s "$API_URL")
 
 get_value() {
-    echo $1 | 
+    echo "$1" | 
     sed 's/^\[{\(.*\)}]$/\1/' | tr ',' '\n' |
-    egrep $2 |
+    grep -E "$2" |
     sed 's/^.*://' | tr -d '"'
 }
 
-ASK_SIZE=$(get_value $JSON "askSize")
-BID_SIZE=$(get_value $JSON "bidSize")
-ASK_PRICE=$(get_value $JSON "askPrice")
-BID_PRICE=$(get_value $JSON "bidPrice")
+ASK_SIZE=$(get_value "$JSON" "askSize")
+BID_SIZE=$(get_value "$JSON" "bidSize")
+ASK_PRICE=$(get_value "$JSON" "askPrice")
+BID_PRICE=$(get_value "$JSON" "bidPrice")
 MID_PRICE=$(echo "scale = 1; ($ASK_PRICE + $BID_PRICE) / 2" | bc)
 
-echo "\xE2\x82\xBF $MID_PRICE"
+printf "\\xE2\\x82\\xBF %s\\n" "$MID_PRICE"
 echo "---"
 
-echo "Ask:\t$ASK_PRICE\tSize: $ASK_SIZE |color=green"
-echo "Bid:\t$BID_PRICE\tSize: $BID_SIZE |color=red"
+printf "Ask:\\t%s\\tSize: %s |color=green\\n" "$ASK_PRICE" "$ASK_SIZE"
+printf "Bid:\\t%s\\tSize: %s |color=red\\n" "$BID_PRICE" "$BID_SIZE"
 echo "---"
 
 echo "Open bitmex.com | href=\"https://www.bitmex.com\""
