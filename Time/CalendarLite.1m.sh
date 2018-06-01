dd03334336f273431f542fa729ff6a70c6827bc4
Weibing Chen <weibing.chen.xy@gmail.com>
2018-06-01 13:02:44 -0700

    Fix typos and shellcheck

--- Time/CalendarLite.1m.sh
+++ Time/CalendarLite.1m.sh
@@ -3,7 +3,7 @@
 # <bitbar.title>Clock with calendar</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Weibing Chen</bitbar.author>
-# <bitbar.author.github>WeibingChen17<bitbar.author.github>
+# <bitbar.author.github>WeibingChen17</bitbar.author.github>
 # <bitbar.desc>A clock with a simple calendar</bitbar.desc>
 # <bitbar.image>http://i65.tinypic.com/260sz1t.png</bitbar.image>
 # <bitbar.dependencies>bash</bitbar.dependencies>
@@ -21,16 +21,16 @@ color="red"
 
 #Comment out these lines to remove "last month"
 last_m=$(date -v-1m +%m)
-last_m_name=$(date -jf %Y-%m-%d $year-$last_m-01 '+%b')
-echo 'Last month: ' "$last_m_name", "$year" '|trim=false font=$font '
+last_m_name=$(date -jf %Y-%m-%d "$year"-"$last_m"-01 '+%b')
+echo 'Last month: ' "$last_m_name", "$year" "|trim=false font=$font"
 cal -d "$year"-"$last_m" |awk 'NF'|sed 's/ *$//'| while IFS= read -r i; do echo "--$i|trim=false font=$font"; done 
 echo "---"
 
-cal |awk 'NF'|sed 's/ $//' |while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/'  ; done
+cal |awk 'NF'|sed 's/ $//' |while IFS= read -r i; do echo " $i|trim=false font=$font color=$color"|  perl -pe '$b="\b";s/ _$b(\d)_$b(\d) /(\1\2)/' |perl -pe '$b="\b";s/_$b _$b(\d) /(\1)/' ; done
 
 #Comment out these lines to remove "next month"
 echo "---"
 next_m=$(date -v+1m +%m)
-next_m_name=$(date -jf %Y-%m-%d $year-$next_m-01 '+%b')
-echo 'Next month: ' "$next_m_name", "$year" '|trim=false font=$font '
+next_m_name=$(date -jf %Y-%m-%d "$year"-"$next_m"-01 '+%b')
+echo 'Next month: ' "$next_m_name", "$year" "|trim=false font=$font"
 cal -d "$year"-"$next_m" | awk 'NF'|sed 's/ *$//' | while IFS= read -r i; do echo "--$i|trim=false font=$font";done
