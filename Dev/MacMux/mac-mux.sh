37c789cbd2a70d9a049cbc04c7e27aa42a2077c8
etopiei <lj343@icloud.com>
2018-06-24 23:22:09 +1000

    Modified script to pass tests

--- Dev/MacMux/mac-mux.sh
+++ Dev/MacMux/mac-mux.sh
@@ -2,21 +2,23 @@
 
 # <bitbar.title>Mac-Mux</bitbar.title>
 # <bitbar.version>v0.1</bitbar.version>
+# <bitbar.image>https://i.imgur.com/7vjC2UU.jpg</bitbar.image>
 # <bitbar.author>etopiei</bitbar.author>
 # <bitbar.author.github>etopiei</bitbar.author.github>
 # <bitbar.desc>This plugin makes it wasy to manage tmux from the menu bar.</bitbar.desc>
 # <bitbar.dependencies>bash</bitbar.dependencies>
 
+# shellcheck source=/dev/null
 source ~/.bash_profile
 if [ "$1" = 'opensession' ]; then
 		command='tmux attach -t '"$2"''
-		$(osascript -e 'tell application "Terminal" to do script "'"$command"'"')
-		$(osascript -e 'set window 1 of application "Terminal" to frontmost')
+		osascript -e 'tell application "Terminal" to do script "'"$command"'"'
+		osascript -e 'set window 1 of application "Terminal" to frontmost'
 fi
 
 if [ "$1" = 'newsession' ]; then
-		$(osascript -e 'tell application "Terminal" to do script "tmux"')
-		$(osascript -e 'tell application "Terminal" activate')
+		osascript -e 'tell application "Terminal" to do script "tmux"'
+		osascript -e 'tell application "Terminal" activate'
 fi
 
 output=$(tmux list-sessions &> /dev/null && tmux list-session | wc -l || echo "0")
@@ -24,7 +26,7 @@ number=$(echo "$output" | xargs)
 
 if [ "$number" != '0' ]; then
 	# here get all the session names in an array
-	nameArray=($(echo "$(tmux list-sessions)" | awk 'BEGIN{FS=":"}{print $1}'))
+	nameArray=($(tmux list-sessions | awk 'BEGIN{FS=":"}{print $1}'))
 	echo "mac-mux ($number)"
 	echo "---"
 	echo "$number Running tmux sessions | color=white"
