c1ba328b391de3c9610ce34c2b75f2d7bb7b271c
clip <binderclip2014@gmail.com>
2018-08-10 19:09:24 +0800

    use python2, add demo image

--- Lifestyle/text_on_menubar.1h.py
+++ Lifestyle/text_on_menubar.1h.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python3
+#!/usr/bin/env PYTHONIOENCODING=UTF-8 /usr/local/bin/python
 # <bitbar.title>Text on Menubar</bitbar.title>
 # <bitbar.version>1.0</bitbar.version>
 # <bitbar.author>clip</bitbar.author>
 # <bitbar.author.github>binderclip</bitbar.author.github>
 # <bitbar.desc>Show your text on menubar</bitbar.desc>
-# <bitbar.dependencies>python3</bitbar.dependencies>
+# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.image>https://qn.cdn.cliiip.com/imgs/u/d93e6b5b-52b7-4324-aaef-8969915f91b1.png</bitbar.image>
+
 import argparse
 import os
 import subprocess
@@ -28,29 +30,32 @@ def read_and_print():
     text_file = get_text_file()
     text = ''
     try:
-        with open(text_file, 'r', encoding='UTF-8') as f:
+        with open(text_file, 'r') as f:
             text = f.read()
-    except FileNotFoundError:
+    except IOError:
         pass
     print(text.strip() or 'Hello')
 
 
 def set_text():
-    ret = subprocess.run(
-        [
-            'osascript',
-            '-e',
-            r'set input_text to text returned of (display dialog "Please input text here:"'
-            ' default answer "" with title "Set the Text")',
-        ], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-    text = ret.stdout.strip().decode('utf-8')
+    try:
+        ret = subprocess.check_output(
+            [
+                'osascript',
+                '-e',
+                r'set input_text to text returned of (display dialog "Please input text here:"'
+                ' default answer "" with title "Set the Text")',
+            ])
+        text = ret.strip()
 
-    text_file = get_text_file()
-    with open(text_file, 'w', encoding='utf-8') as f:
-        f.write(text)
+        text_file = get_text_file()
+        with open(text_file, 'w') as f:
+            f.write(text)
+    except subprocess.CalledProcessError:
+        pass
     # refresh
     s = "bitbar://refreshPlugin?name={}".format(get_file_name())
-    subprocess.run(['open', s])
+    subprocess.call(['open', s])
 
 
 def print_submenu():
@@ -60,7 +65,8 @@ def print_submenu():
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-s", "--set_text", action="store_true", help='set the text')
+    parser.add_argument("-s", "--set_text",
+                        action="store_true", help='set the text')
     args = parser.parse_args()
 
     if args.set_text:
