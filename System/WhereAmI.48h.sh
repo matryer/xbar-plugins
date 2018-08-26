4e99bc5eaf4e9f7d026dd0121f393ff714459614
noyannus <terw@gmx.de>
2018-08-26 19:42:56 +0200

    Fix image link

--- System/WhereAmI.48h.sh
+++ System/WhereAmI.48h.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 # <bitbar.title>WhereAmI</bitbar.title>
-# <bitbar.version>v1</bitbar.version>
+# <bitbar.version>v1.0.1</bitbar.version>
 # <bitbar.author>noyannus</bitbar.author>
 # <bitbar.author.github>noyannus</bitbar.author.github>
 # <bitbar.desc>Displays current system volume, system version, and user's home folder.
 # <bitbar.desc>Useful for staying oriented while booting into multiple test systems.</bitbar.desc>
 # <bitbar.desc>Readme: https://github.com/noyannus/WhereAmI-Readme/blob/master/Readme.md</bitbar.desc>
-# <bitbar.image>https://github.com/noyannus/WhereAmI-Readme/blob/master/Minimalist.png</bitbar.image>
-# <bitbar.image>https://github.com/noyannus/WhereAmI-Readme/blob/master/KingSizeInfo.png</bitbar.image>
+# <bitbar.image>https://raw.githubusercontent.com/noyannus/WhereAmI-Readme/master/Minimalist.pngg</bitbar.image>
+# <bitbar.image>https://raw.githubusercontent.com/noyannus/WhereAmI-Readme/master/KingSizeInfo.png</bitbar.image>
 
 
 # Use the last `cut` to get only the relevant part of something like "High Sierra beta with Spaghetticode Fork #123 Version 456alpha"
@@ -30,4 +30,6 @@ echo "---"
 echo "Boot Volume:        $BOOTVOLUME | size=18"    color=black
 echo "System Version:   $SYSTEMVERSION | size=18" color=black
 echo "User Directory:     $USERDIR | size=18"       color=black
+
+
 
