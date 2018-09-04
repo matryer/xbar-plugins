6ac37fe3092e78d19188d9abe97fb6bc591ef14d
hypersport <boss.yuan@outlook.com>
2018-09-04 23:46:56 +0800

    fix the shellcheck issues

--- System/top5-memory-consuming-processes.sh
+++ System/top5-memory-consuming-processes.sh
@@ -8,15 +8,15 @@
 # <bitbar.image>http://23.105.211.229/images/top5-memory-consuming.jpg</bitbar.image>
 # <bitbar.about>top 5 memory-consuming applications</bitbar.about>
 
-ps xmo rss=,pmem=,comm= | while read rss pmem comm; ((n++<5)); do
-size="$[rss/1024]";
-short=$[4-${#size}];
+ps xmo rss=,pmem=,comm= | while read -r rss pmem comm; ((n++<5)); do
+size="$((rss/1024))";
+short=$((4-${#size}));
 size="(${size}M)";
 i=0;
 while ((i++ < short)); do size=" $size"; done;
 pmem="${pmem%%.*}"
-if (($pmem >= 20 )); then color=$'\e[31m';
-elif (($pmem >= 10)); then color=$'\e[33m';
+if [ "$pmem" -ge 20 ]; then color=$'\e[31m';
+elif [ "$pmem" -ge 10 ]; then color=$'\e[33m';
 else color=$'\e[32m';
 fi
 echo "$color$pmem% $size $(basename "$comm")"$'\e[0m'"";
