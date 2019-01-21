62ffe3743954cda96448db5826c12b19c03af202
Sean Luce <seanluce@Seans-MacBook-Pro.local>
2019-01-20 22:01:53 -0500

    Fixed errors with code formatting.

--- AWS/awsmonthcost.1h.sh
+++ AWS/awsmonthcost.1h.sh
@@ -10,7 +10,7 @@
 
 start=$(date -v1d +%Y-%m-%d)
 end=$(date -v+1m -v1d -v-1d +%Y-%m-%d)
-export PATH='/usr/local/bin:/usr/bin:$PATH'
-prefix=$(aws ce get-cost-and-usage --time-period Start=$start,End=$end --granularity MONTHLY --metric "BlendedCost" | awk '/Unit/ {print $2;}' | sed 's/[,"]//g')
-cost=$(aws ce get-cost-and-usage --time-period Start=$start,End=$end --granularity MONTHLY --metric "BlendedCost" | awk '/Amount/ {print $2;}' | sed 's/[,"]//g' | xargs printf "%.*f\n" 2)
-echo $cost $prefix
+export PATH="/usr/local/bin:/usr/bin:$PATH"
+prefix=$(aws ce get-cost-and-usage --time-period Start="$start",End="$end" --granularity MONTHLY --metric "BlendedCost" | awk '/Unit/ {print $2;}' | sed 's/[,"]//g')
+cost=$(aws ce get-cost-and-usage --time-period Start="$start",End="$end" --granularity MONTHLY --metric "BlendedCost" | awk '/Amount/ {print $2;}' | sed 's/[,"]//g' | xargs printf "%.*f\n" 2)
+echo "$cost" "$prefix"
