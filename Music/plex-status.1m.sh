b03c45b830618d035c2b3c9e87b0a07cf1866024
Chris Bergeron <cbergeron@gmail.com>
2019-02-23 14:41:54 -0500

    Update plex-status.1m.sh

--- Music/plex-status.1m.sh
+++ Music/plex-status.1m.sh
@@ -10,14 +10,14 @@
 
 # Put your plex hostname here
 plexhost="plex-01"
-curlcmd=$(command -v curl)
+#curlcmd=$(command -v curl)
 
 # Put host to ping here (useful for vpn connections)
 pinghost="plex-01"
 
 # To do: Automate this from a curl response
 # The UUID of your Plex Server.  This can be obtained from the browser address bar after selecting an item (it's between /server/ and /detail/)
-serverid="867c53098dc5feccea1cb108c18448cd6af92"
+serverid="YOUR SERVER ID HERE"
 
 # Temp filename
 tmpfile="/tmp/image.jpg"
@@ -33,7 +33,7 @@ echo > /tmp/output.txt
 plextoken=YOUR_PLEX_API_TOKEN_HERE
 
 # If host is unreachable, display message and stop
-if [ /sbin/ping -c1 $plexhost > /dev/null 2>&1 -ne 0 ]; then
+if ! (ping -c1 -W1 $plexhost > /dev/null 2>&1); then
   echo "❌"
   echo "---"
   echo "Can't connect to $pinghost"
@@ -41,25 +41,29 @@ if [ /sbin/ping -c1 $plexhost > /dev/null 2>&1 -ne 0 ]; then
 fi
 
 # If we can connect to Plex, let's see what's playing and display it
-if [ $curlcmd -s http://$plexhost:32400/identity > /dev/null 2>&1 -ne 0 ]; then
+if ! (curl -s http://$plexhost:32400/identity > /dev/null 2>&1); then
   echo "❌"
   echo "---"
   echo "Plex is unreachable | color=red"
   echo "$plexhost"
 fi
 
-# get our session data from Plex
+# Get our session data from Plex
 xmldata=$(curl -s -X GET \
-  'http://plex-01:32400/status/sessions?X-Plex-Token='"$plextoken" \
+  "http://plex-01:32400/status/sessions?X-Plex-Token=$plextoken" \
   -H 'Content-Length: 0' \
   -H 'X-Plex-Client-Identifier: my-app' \
   -H 'cache-control: no-cache')
 
-#echo "XMLDATA is: ($xmldata)" >> /tmp/output.txt
+echo "XMLDATA is: ($xmldata)" >> /tmp/output.txt
 
+if (echo "$xmldata" | grep "Unauthorized"> /dev/null 2>&1); then
+  echo -en "❌\n---\nUnauthorized.\nCheck your API Key."
+  exit
+fi
+  
 # Check to see if anything is playing, if not, say so and exit
-echo $xmldata | grep "Video" > /dev/null 2>&1
-if [ $? -ne 0 ]; then
+if ! (echo "$xmldata" | grep "Video" > /dev/null 2>&1); then
   echo "○"
   echo "---"
   echo "Nothing playing"
@@ -68,7 +72,7 @@ fi
 
 # Check to see if a trailer is playing, if so, exit
 # To do: Display trailer info
-if [ echo "$xmldata" | grep "subtype=\"trailer\"" > /dev/null 2>&1 -eq 0 ]; then
+if ! (echo "$xmldata" | grep "subtype=\"trailer\"" > /dev/null 2>&1); then
   echo "D"
   echo "---"
   echo "Preview Trailer Playing"
@@ -76,32 +80,36 @@ if [ echo "$xmldata" | grep "subtype=\"trailer\"" > /dev/null 2>&1 -eq 0 ]; then
 fi
 
 # If metadata is a movie, parse it separately
-if [ echo "$xmldata" | grep "movie" > /dev/null 2>&1 -eq 0 ]; then
+if (echo "$xmldata" | grep "movie" > /dev/null 2>&1); then
   # We're watching a movie, parse as such
   thumblink=$(echo "$xmldata" | sed -e 's/>/\>\n/g' | head -n3 | tail -n1 | cut -f4 -d "\"" | sed -e 's/art/thumb/g')
   keydata="%2Flibrary%2Fmetadata%2F"$(echo "$thumblink" | cut -f4 -d "/")
 else
   # We're watching TV, parse as such
-  thumblink=$(echo "$xmldata" | /usr/local/bin/sed -e 's/" /\n/g' | /usr/bin/grep grandparentThumb | cut -f2 -d "\""| /usr/bin/head -n1)
-  keydata=$(echo "$xmldata" | /usr/local/bin/sed -e 's/" /\n/g' | /usr/bin/grep "key" | head -n1 | cut -f2 -d "\"" | sed -e 's/\//%2F/g' | /usr/bin/head -n1)
+  thumblink=$(echo "$xmldata" | sed -e 's/" /\n/g' | grep grandparentThumb | cut -f2 -d "\""| head -n1)
+  keydata=$(echo "$xmldata" | sed -e 's/" /\n/g' | grep "key" | head -n1 | cut -f2 -d "\"" | sed -e 's/\//%2F/g' | head -n1)
 fi
 
 # Debug info
-echo "thumbnail link is: ($thumblink)" >> /tmp/output.txt
-echo "keydata for url is: ($keydata)" >> /tmp/output.txt
+#echo "thumbnail link is: ($thumblink)" >> /tmp/output.txt
+#echo "keydata for url is: ($keydata)" >> /tmp/output.txt
 
 curl -s -o /tmp/image.jpg -X GET \
-  'http://plex-01:32400$thumblink?X-Plex-Token='"$plextoken" \
+  "http://plex-01:32400$thumblink?X-Plex-Token=$plextoken" \
   -H 'Content-Length: 0' \
   -H 'X-Plex-Client-Identifier: my-app' \
   -H 'cache-control: no-cache'
 
 # Resize our image to a small thumbnail
-#/usr/local/bin/mogrify -scale 75x100 $tmpfile
-/usr/local/bin/mogrify -scale 75x100 /tmp/image.jpg >> /tmp/output.txt 2>&1
+#mogrify -scale 75x100 $tmpfile
+if ! (mogrify -scale 75x100 /tmp/image.jpg >> /tmp/output.txt 2>&1); then
+  echo "---"
+  echo "Couldn't render thumbnail image"
+  exit
+fi
 
 # Encode our image into base64 text for BitBar
-imgstr=$(/usr/bin/base64 /tmp/image.jpg)
+imgstr=$(base64 /tmp/image.jpg)
 
 # Display our menu output
 echo "| templateImage=iVBORw0KGgoAAAANSUhEUgAAABAAAAAQCAQAAAC1+jfqAAAABGdBTUEAALGPC/xhBQAAACBjSFJNAAB6JgAAgIQAAPoAAACA6AAAdTAAAOpgAAA6mAAAF3CculE8AAAAAmJLR0QA/4ePzL8AAAAJcEhZcwAAC4kAAAuJATfJy60AAAAHdElNRQfjAhMPKzgxRmpXAAAAuUlEQVQoz5XRMU4CYRQE4O93LbCFtZDQ2dGItS2n4AKchFNwAbnIVhYmW1GTEBJDgiGRbh+FGyPwR+I0r5iZvHlvgGSmEcLeUBalWghhofubKNr55dNYBw823sW5gLW+Z0lH6c3mUnCw8mQgGbhVOZwL+FB4cSd5tFTnonYt2qi1Mn/N0F4IjZkEN/6FKysKU1sh7Exy/pFKIzTmepd0z7ylK6Ocf2InhK3pyXda3P+U9Xpa1jf+qPsIyJ5B7rJwvZwAAAAldEVYdGRhdGU6Y3JlYXRlADIwMTktMDItMTlUMjA6NDQ6MDAtMDU6MDBDoNoZAAAAJXRFWHRkYXRlOm1vZGlmeQAyMDE5LTAyLTE5VDIwOjQzOjU2LTA1OjAw+xFCggAAAABJRU5ErkJggg=="
