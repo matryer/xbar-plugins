489fdc250e02877f1b35a25da0f63f5e4c73f3a1
Bryan Stone <aegixx@gmail.com>
2019-08-25 19:16:28 -0500

    Fix lint issues
    Signed-off-by: Bryan Stone <aegixx@gmail.com>

--- Network/wifisignal.sh
+++ Network/wifisignal.sh
@@ -8,14 +8,14 @@
 # Themes copied from here: http://colorbrewer2.org/
 # shellcheck disable=SC2034
 RED_GREEN_THEME=("#d73027" "#fc8d59" "#fee08b" "#d9ef8b" "#91cf60" "#1a9850")
-COLORS=(${RED_GREEN_THEME[@]})
+COLORS=("${RED_GREEN_THEME[@]}")
 
 WIFIDATA=$(/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I)
 SSID=$(echo "$WIFIDATA" | awk '/ SSID/ {print substr($0, index($0, $2))}')
 SIGNAL=$(echo "$WIFIDATA" | awk '/ agrCtlRSSI/ {print substr($0, index($0, $2))}')
 NOISE=$(echo "$WIFIDATA" | awk '/ agrCtlNoise/ {print substr($0, index($0, $2))}')
 
-SNR=$(($SIGNAL - $NOISE))
+SNR=$(("$SIGNAL" - "$NOISE"))
 
 # Signal Strength – 0dBm (strongest) and --100dBm (weakest). 
 ## -30 dBm  Amazing
@@ -25,25 +25,25 @@ SNR=$(($SIGNAL - $NOISE))
 ## -70 dBm	Okay
 ## -80 dBm	Not Good
 ## -90 dBm	Unusable
-if (($SIGNAL >= -30)); then
+if (("$SIGNAL" >= -30)); then
     RATING="Amazing"
     COLOR=${COLORS[6]}
-elif (($SIGNAL >= -50)); then
+elif (("$SIGNAL" >= -50)); then
     RATING="Excellent"
     COLOR=${COLORS[5]}
-elif (($SIGNAL >= -60)); then
+elif (("$SIGNAL" >= -60)); then
     RATING="Good"
     COLOR=${COLORS[4]}
-elif (($SIGNAL >= -67)); then
+elif (("$SIGNAL" >= -67)); then
     RATING="Reliable"
     COLOR=${COLORS[3]}
-elif (($SIGNAL >= -70)); then
+elif (("$SIGNAL" >= -70)); then
     RATING="Okay"
     COLOR=${COLORS[2]}
-elif (($SIGNAL >= -80)); then
+elif (("$SIGNAL" >= -80)); then
     RATING="Not Good"
     COLOR=${COLORS[1]}
-elif (($SIGNAL >= -90)); then
+elif (("$SIGNAL" >= -90)); then
     RATING="Unusable"
     COLOR=${COLORS[0]}
 else
@@ -56,7 +56,7 @@ fi
 ## Medium quality: 50% ~= -75dBm
 ## Low quality: 30% ~= -85dBm
 ## Unusable quality: 8% ~= -96dBm
-QUALITY=$((2 * $SNR))
+QUALITY=$((2 * "$SNR"))
 QUALITY=$((QUALITY < 100 ? QUALITY : 100))
 
 echo "📶 $SSID | color=$COLOR"
