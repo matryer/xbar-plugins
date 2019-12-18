b66159065af8cfcf07787e1984250cd5d7889744
cghamburg <cghamburg@users.noreply.github.com>
2019-12-18 15:12:24 +0100

    Add script to print number of running Docker containers

---
+++ Dev/Docker/numContainers.5s.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# <bitbar.title>NumContainers</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>cghamburg</bitbar.author>
+# <bitbar.author.github>cghamburg</bitbar.author.github>
+# <bitbar.desc>Print number of running Docker containers with whale unicode char</bitbar.desc>
+# <bitbar.dependencies>docker</bitbar.dependencies>
+
+CONTAINERS=$(/usr/local/bin/docker ps --format '{{.Names}}' | sort)
+NUM_CONTAINERS=$(echo "${CONTAINERS}" | wc -l)
+
+echo "$(printf "🐳 %.0f \n" "${NUM_CONTAINERS}") | size=13"
+echo "---"
+echo "${CONTAINERS}"
