e2aca748dd4df30e8345d1b6ec31b6e5b274e378
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-01-21 17:54:05 -0700

    Fixup shellcheck linters

--- System/disk-read-write.1s.sh
+++ System/disk-read-write.1s.sh
@@ -10,9 +10,9 @@
 
 # BitBar Disk IO plugin
 
-STAT=`iostat -dc 2 disk0`
-RATE=`echo "$STAT"| tail -n1 | rev | cut -d ' ' -f2 | rev`
-ABSOLUTE_UNIT=`echo "$STAT"| head -n2 | tail -n1 | rev | cut -d ' ' -f2 | rev`
+STAT=$(iostat -dc 2 disk0)
+RATE=$(echo "$STAT"| tail -n1 | rev | cut -d ' ' -f2 | rev)
+ABSOLUTE_UNIT=$(echo "$STAT"| head -n2 | tail -n1 | rev | cut -d ' ' -f2 | rev)
 echo "🏃$RATE $ABSOLUTE_UNIT";
 echo "---";
 echo "$STAT"
