d56e8572d2df74dac0e2e2cf4f3ae57c98189e76
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-01-24 12:29:54 -0700

    Cleanup duplicate weather script

--- Weather/OpenWeatherMap/weather.15m.py
+++ Weather/OpenWeatherMap/weather.15m.py
@@ -2,12 +2,13 @@
 # -*- coding: utf-8 -*-
 
 # <bitbar.title>Weather - OpenWeatherMap</bitbar.title>
-# <bitbar.version>v1.0.2</bitbar.version>
-# <bitbar.author>Daniel Seripap</bitbar.author>
-# <bitbar.author.github>seripap</bitbar.author.github>
+# <bitbar.version>v1.2</bitbar.version>
+# <bitbar.author>rmwphd</bitbar.author>
+# <bitbar.author.github>rmwphd</bitbar.author.github>
 # <bitbar.desc>Grabs simple weather information from openweathermap. Needs configuration for location and API key.</bitbar.desc>
-# <bitbar.dependencies>python</bitbar.dependencies>
+# <bitbar.dependencies>python,emoji</bitbar.dependencies>
 
+import emoji
 import json
 import urllib2
 from random import randint
@@ -38,6 +39,7 @@ def get_wx():
     weather_data = {
       'temperature': str(int(round(wx['main']['temp']))),
       'condition': str(wx['weather'][0]['description'].encode('utf-8')),
+      'id': wx['weather'][0]['id'].encode('utf-8'),
       'city': wx['name'],
       'unit': '°' + unit
     }
@@ -48,10 +50,26 @@ def get_wx():
 
 def render_wx():
   weather_data = get_wx()
+  emoji_dict = {
+  200 : ":zap:",  201 : ":zap:",  202 : ":zap:",  210 : ":zap:",  211 : ":zap:",  212 : ":zap:",  221 : ":zap:",  230 : ":zap:",  231 : ":zap:",  232 : ":zap:",
+  300 : ":umbrella:",  301 : ":umbrella:",  302 : ":umbrella:",  310 : ":umbrella:",  311 : ":umbrella:",  312 : ":umbrella:",  313 : ":umbrella:",  314 : ":umbrella:",  321 : ":umbrella:",
+  500 : ":umbrella:",  501 : ":umbrella:",  502 : ":umbrella:",  503 : ":umbrella:",  504 : ":umbrella:",  511 : ":umbrella:",  520 : ":umbrella:",  521 : ":umbrella:",  522 : ":umbrella:",  531 : ":umbrella:",
+  600 : ":snowflake:",  601 : ":snowflake:",  602 : ":snowflake:",  611 : ":snowflake:",  612 : ":snowflake:",  613 : ":snowflake:",  615 : ":snowflake:",  616 : ":snowflake:",  620 : ":snowflake:",  621 : ":snowflake:",  622 : ":snowflake:",
+  701 : ":fog:",  711 : ":fog:",  721 : ":fog:",  731 : ":fog:",  741 : ":fog:",  751 : ":fog:",  761 : ":fog:",  762 : ":fog:",  771 : ":fog:",
+  781 : ":cyclone:",
+  800 : ":sunny:",
+  801 : ":partly_sunny:",  802 : ":partly_sunny:",  803 : ":cloud:",  804 : ":cloud:",
+  }
+  tridash = '\n' + '---' + '\n'
 
   if weather_data is False:
-    return 'Could not get weather'
+    return 'Err' + tridash + 'Could not get weather; Maybe check API key or location?'
 
-  return weather_data['condition'] + ' ' + weather_data['temperature'] + weather_data['unit']
+  emojiweather = emoji.emojize(emoji_dict[weather_data['id']])
 
-print render_wx()
+  #weather_data['condition'] + ' ' +
+  emoji_t = '' + emojiweather + weather_data['temperature'] + weather_data['unit']
+  condi = [x.capitalize() for x in  weather_data['condition'].split(' ')]
+  return emoji_t + tridash + ' '.join(condi) + ' | refresh = true'
+
+print(render_wx())
