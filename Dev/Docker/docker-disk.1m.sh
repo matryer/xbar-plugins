0660e1f54b74ec32602a89347381026095664b88
Horatiu Ion <dev@horatiuion.com>
2020-01-24 15:14:26 +0100

    fix linter issues

--- Dev/Docker/docker-disk.1m.sh
+++ Dev/Docker/docker-disk.1m.sh
@@ -11,13 +11,13 @@
 #
 # Displays the total disk usage of docker. Clicking the menu will show a detailed breakdown (images, containers, volumes, build cache)
 
-export PATH='/usr/local/bin:/usr/bin:$PATH'
-DOCKER="$(which docker)"
+export PATH="/usr/local/bin:/usr/bin:$PATH"
+DOCKER="$(command -v docker)"
 
 humanize() {
     local L_BYTES="${1:-0}"
     local L_BASE="${2:-1000}"
-    echo $1 | awk -v bytes="${L_BYTES}" -v base="${L_BASE}" 'function human(x, base) {
+    echo "$1" | awk -v bytes="${L_BYTES}" -v base="${L_BASE}" 'function human(x, base) {
          if(base!=1024)base=1000
          basesuf=(base==1024)?"iB":"B"
 
@@ -35,32 +35,31 @@ humanize() {
 }
 
 dehumanise() {
-    for v in "${@:-$(</dev/stdin)}"; do
-        echo $v | awk \
-            'BEGIN{IGNORECASE = 1}
+    read -r v
+    echo "$v" | awk \
+        'BEGIN{IGNORECASE = 1}
         function printpower(n,b,p) {printf "%d\n", n*b^p}
         /[0-9](B)?$/{ printpower($1, 10,  1); next;};
         /K(B)?$/{     printpower($1, 10,  3); next;};
         /M(B)?$/{     printpower($1, 10,  6); next;};
         /G(B)?$/{     printpower($1, 10,  9); next;};
         /T(B)?$/{     printpower($1, 10, 12); next;};'
-    done
 }
 
 DOCKER_DF=$(${DOCKER} system df --format "{{.Size}}")
 
-IMAGE_SIZES=$(echo ${DOCKER_DF} | cut -d " " -f 1 | dehumanise)
-CONTAINER_SIZES=$(echo ${DOCKER_DF} | cut -d " " -f 2 | dehumanise)
-VOLUME_SIZES=$(echo ${DOCKER_DF} | cut -d " " -f 3 | dehumanise)
-BUILD_SIZES=$(echo ${DOCKER_DF} | cut -d " " -f 4 | dehumanise)
+IMAGE_SIZES=$(echo "${DOCKER_DF}" | sed -n 1p | dehumanise)
+CONTAINER_SIZES=$(echo "${DOCKER_DF}" | sed -n 2p | dehumanise)
+VOLUME_SIZES=$(echo "${DOCKER_DF}" | sed -n 3p | dehumanise)
+BUILD_SIZES=$(echo "${DOCKER_DF}" | sed -n 4p | dehumanise)
 
 TOTAL=$((IMAGE_SIZES + CONTAINER_SIZES + VOLUME_SIZES + BUILD_SIZES))
 
 echo "🐳: $(humanize ${TOTAL})"
 echo "---"
-echo "images: $(echo ${DOCKER_DF} | cut -d " " -f 1)"
-echo "containers: $(echo ${DOCKER_DF} | cut -d " " -f 2)"
-echo "volumes: $(echo ${DOCKER_DF} | cut -d " " -f 3)"
-echo "build cache: $(echo ${DOCKER_DF} | cut -d " " -f 4)"
+echo "images: $(echo "${DOCKER_DF}" | sed -n 1p)"
+echo "containers: $(echo "${DOCKER_DF}" | sed -n 2p)"
+echo "volumes: $(echo "${DOCKER_DF}" | sed -n 3p)"
+echo "build cache: $(echo "${DOCKER_DF}" | sed -n 4p)"
 echo "---"
 echo "Refresh | refresh=true"
