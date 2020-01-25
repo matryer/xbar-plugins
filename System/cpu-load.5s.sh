9804ee61aa7a39c3cc70660f7edb6df3ba8a8695
Paul W. Rankin <code@paulwrankin.com>
2020-01-25 19:10:08 +1000

    Use imgur for @rnkn plugins

--- System/cpu-load.5s.sh
+++ System/cpu-load.5s.sh
@@ -5,7 +5,7 @@
 # <bitbar.author>Paul W. Rankin</bitbar.author>
 # <bitbar.author.github>rnkn</bitbar.author.github>
 # <bitbar.desc>Shows CPU load as a percentage (without using top).</bitbar.desc>
-# <bitbar.image>https://f002.backblazeb2.com/file/pwr-share/cpu-load.png</bitbar.image>
+# <bitbar.image>https://i.imgur.com/B6VAsDg.png</bitbar.image>
 # <bitbar.dependencies>bash</bitbar.dependencies>
 
 # BitBar CPU Load plugin
