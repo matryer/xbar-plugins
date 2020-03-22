9c820e0633521cd2c50d5503996d340a794cf88f
jhaydraude <jhaydraude@salesforce.com>
2020-03-21 23:49:54 -0400

    Update nightscout plugin to use JSON (#1391)
    * Update ns plugin to use JSON
    
    * Fix Nightscout Plugin lint errors

--- Lifestyle/nightscout.1m.sh
+++ Lifestyle/nightscout.1m.sh
@@ -1,32 +1,45 @@
 #!/bin/bash
 # <bitbar.title>Nightscout Reader</bitbar.title>
-# <bitbar.version>0.1.0</bitbar.version>
+# <bitbar.version>0.2.0</bitbar.version>
 # <bitbar.desc>For Diabetics using Nightscout to track CGM data: Display current Blood Sugar data and trend from Nightscout</bitbar.desc>
-# <bitbar.dependencies>bash, curl, bc</bitbar.dependencies>
+# <bitbar.dependencies>bash, curl, bc, jq</bitbar.dependencies>
 # <bitbar.author>Jeremy Hay Draude</bitbar.author>
-# <bitbar.author.github>badgerpapa</bitbar.author.github>
-# <bitbar.image>https://raw.githubusercontent.com/badgerpapa/NightscoutBitBar/master/Preview.png</bitbar.image>
-# <bitbar.abouturl>https://github.com/badgerpapa/NightscoutBitBar/blob/master/README.md</bitbar.abouturl>
+# <bitbar.author.github>jhaydraude</bitbar.author.github>
+# <bitbar.image>https://raw.githubusercontent.com/jhaydraude/NightscoutBitBar/master/Preview.png</bitbar.image>
+# <bitbar.abouturl>https://github.com/jhaydraude/NightscoutBitBar/blob/master/README.md</bitbar.abouturl>
 
-NSURL=http://YOURNIGHTSCOUTURL.herokuapp.com # Add your own Nightscout URL here
+export PATH="/usr/local/bin:/usr/bin:$PATH"
+
+if ! [ -x "$(command -v jq)" ]; then
+  echo 'Error: jq is not installed.'
+  echo '---'
+  echo 'Click here to install jq | href=https://stedolan.github.io/jq/download/'
+  exit 1
+fi
+
+NSURL=https://YOURNIGHTSCOUTURL.herokuapp.com # Add your own Nightscout URL here
 USEMMOL=true # true if you use mmol/l units. false if you use mg/dl
 
+JSONOUT=$(curl --silent $NSURL/api/v1/entries/current.json)
 
-read -r -a RESULTARRAY <<< "$(curl --silent $NSURL/api/v1/entries/current)"
+JSDATE=$(jq '.[0].date' <<< "$JSONOUT")
+#NS Returns date as ms since epoch. BASH likes Seconds since epoch
+EPOCHTS=$((JSDATE / 1000))
+TIMESTRING=$(date -r $EPOCHTS)
 
-TIMESTAMP=${RESULTARRAY[0]//\"}  
-TIMESTAMP=${TIMESTAMP//\.[0-9][0-9][0-9]/} #Strip milliseconds from the timestamp. BSD date doesn't like it.
-EPOCHTS=$(date -j -f "%FT%T%z" "$TIMESTAMP" +%s) # Convert timestamp to epoch time
 EPOCHNOW=$(date +%s) # Convert current time to epoch time
 TIMEDIFF=$(((EPOCHNOW - EPOCHTS)/60)) #calculate the difference
 
-BG=${RESULTARRAY[2]}
+BG=$(jq '.[0].sgv' <<< "$JSONOUT")
+
+TRENDSTR=$(jq -r '.[0].direction' <<< "$JSONOUT")
+
 if $USEMMOL ; then
 	BG=$(echo "scale=1; $BG / 18" | bc) #Convert mg/dl to mmol/l
 fi
 
 
-case ${RESULTARRAY[3]//\"} in
+case $TRENDSTR in
 	FortyFiveUp)
 		TREND='↗'
 		;;
@@ -49,11 +62,11 @@ case ${RESULTARRAY[3]//\"} in
 		TREND='⇊'
 		;;
 	*)
-		TREND=${RESULTARRAY[3]}
+		TREND=$TRENDSTR
 		;;
 esac
 
 echo "$BG $TREND (${TIMEDIFF}m ago)"
 echo "---"
 echo "Go to Nightscout | href=$NSURL"
-echo "Reading taken: $TIMESTAMP"
+echo "Reading taken: $TIMESTRING"
