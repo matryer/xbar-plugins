666c9ead06e4f552cf289d2ac6a1d17c75092dc8
Brian Hartvigsen <brian.andrew@brianandjenny.com>
2020-04-09 21:31:47 -0600

    Increment version:

--- Dev/Docker/docker-status.1m.sh
+++ Dev/Docker/docker-status.1m.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #
 # <bitbar.title>Docker Status</bitbar.title>
-# <bitbar.version>v1.1</bitbar.version>
+# <bitbar.version>v1.2</bitbar.version>
 # <bitbar.author>Manoj Mahalingam</bitbar.author>
 # <bitbar.author.github>manojlds</bitbar.author.github>
 # <bitbar.image>https://cloud.githubusercontent.com/assets/191378/12255368/1e671b32-b919-11e5-8166-6d975396f408.png</bitbar.image>
