dde761e0e93fb1e6b6dfcdb7f236ace341958795
CodinCafe <codincafe@gmail.com>
2020-07-01 20:45:19 +0530

    My External Ip
    This plugin will show your current expternal / public IP and allow you to copy the same to clipboard

---
+++ Network/external-ip-1.0.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# <bitbar.title>My External IP</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>CodinCafe</bitbar.author>
+# <bitbar.author.github>codincafe</bitbar.author.github>
+# <bitbar.desc>This plugin will show your current expternal / public IP and allow you to copy the same to clipboard</bitbar.desc>
+# <bitbar.image>https://i.imgur.com/Ar1JABA.png</bitbar.image>
+# <bitbar.dependencies>curl</bitbar.dependencies>
+
+echo "🌎"
+echo '---'
+OUTPUT=$(curl -s https://ipinfo.io/ip)
+
+result_string="$OUTPUT | bash='/bin/bash' param1='-c' param2='/bin/echo $OUTPUT | pbcopy' terminal=false"
+echo "$result_string"
