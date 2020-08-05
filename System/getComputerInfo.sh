8974d8a7091fc170d2a4e5c4183629a5abbaf789
Sarah Keenan <48265613+SKeenan07@users.noreply.github.com>
2020-08-04 21:41:44 -0400

    Create getComputerInfo.sh

---
+++ System/getComputerInfo.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+# <bitbar.title>Get Computer Info</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Sarah Keenan</bitbar.author>
+# <bitbar.author.github>SKeenan07</bitbar.author.github>
+# <bitbar.desc>This plugin gets the IP address and the computer name.</bitbar.desc>
+# <bitbar.image>https://github.com/SKeenan07/portfolio/blob/master/images/BitBarPlugin.png</bitbar.image>
+
+activeNetworkAdapter=$(echo 'show State:/Network/Global/IPv4' | scutil | grep PrimaryInterface | sed 's/ PrimaryInterface : //')
+
+computerName=$(system_profiler SPSoftwareDataType | grep "Computer Name")
+
+batteryCondition=$(system_profiler SPPowerDataType | grep "Condition")
+
+echo "🖥"
+
+echo "---"
+
+echo "${computerName:21}"
+
+echo "---"
+
+# Batery conditions can be Normal, Replace Soon, Replace Now, and Service Battery
+echo "Battery Condition: ${batteryCondition:21}"
+
+echo "---"
+
+# If the active network adapter is empty, then the Mac isn't connected to the internet
+if [[ -z "$activeNetworkAdapter" ]]; then
+    echo "Not connected to the internet."
+else
+    IPAddress=$(ifconfig $activeNetworkAdapter | grep "inet " | awk '{ print $2 }')
+    echo "$IPAddress"
+fi
