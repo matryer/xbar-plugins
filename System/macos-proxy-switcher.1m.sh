99080e74094b1aaa6e60e3d8c9a13bf081498b6a
glowinthedark <48893368+glowinthedark@users.noreply.github.com>
2020-08-31 17:04:38 +0200

    add PAC proxy

--- System/macos-proxy-switcher.1m.sh
+++ System/macos-proxy-switcher.1m.sh
@@ -5,9 +5,9 @@
 # <bitbar.author>glowinthedark</bitbar.author>
 # <bitbar.author.github>glowinthedark</bitbar.author.github>
 # <bitbar.desc>Set http and socks5 proxy settings on MacOS.</bitbar.desc>
-# <bitbar.image>https://raw.githubusercontent.com/glowinthedark/bitbar-plugins/macos-proxy-switcher/images/mac-proxy-switcher.png</bitbar.image>
+# <bitbar.image>https://raw.githubusercontent.com/glowinthedark/bitbar-plugins/macos-proxy-switcher/images/mac-proxy-switcher.png.png</bitbar.image>
 # <bitbar.dependencies></bitbar.dependencies>
-# <bitbar.abouturl>https://github.com/glowinthedark/bitbar-plugins/blob/master/System/macos-proxy-switcher.1m.sh</bitbar.abouturl>
+# <bitbar.abouturl>https://github.com/glowinthedark/bitbar-plugins/System/macos-proxy-switcher.1m.sh</bitbar.abouturl>
 
 # CONFIGURATION
 INTERFACE=Wi-Fi
@@ -17,6 +17,9 @@ SOCKS_PROXY_PORT=1080
 
 HTTP_PROXY_HOST=localhost
 HTTP_PROXY_PORT=8080
+
+PAC_PROXY="file://$HOME/pac_file_proxy.pac"
+
 # END CONFIGURATION
 
 if [[ "$1" = "enable_socks5_proxy" ]]; then
@@ -41,52 +44,78 @@ if [[ "$1" = "disable_http_proxy" ]]; then
   exit
 fi
 
+if [[ "$1" = "enable_pac_proxy" ]]; then
+  networksetup -setautoproxyurl  $INTERFACE $PAC_PROXY
+  networksetup -setautoproxystate $INTERFACE on
+  exit
+fi
+
+if [[ "$1" = "disable_pac_proxy" ]]; then
+  # networksetup -setautoproxyurl $INTERFACE ""
+  networksetup -setautoproxystate $INTERFACE off
+  exit
+fi
+
+
 if [[ "$1" = "edit_this_script" ]]; then
   # use default editor for .sh extension
-  open "$0";
+  # open "$0";
   # explicitly use sublimetext3
-  # open -b com.sublimetext.3 "$0";
+  open -b com.sublimetext.3 "$0";
   exit
 fi
 
 current_socks5_proxy_status=$(networksetup -getsocksfirewallproxy $INTERFACE | awk 'NR=1{print $2; exit}')
 current_http_proxy_status=$(networksetup -getwebproxy $INTERFACE | awk 'NR=1{print $2; exit}')
+current_pac_proxy_status=$(networksetup -getautoproxyurl $INTERFACE | grep Enabled | awk 'NR=1{print $2; exit}')
 
 # SOCK5 PROXY
-if [[ $current_socks5_proxy_status == "Yes" ]] || [[ $current_http_proxy_status == "Yes" ]]; then
-  ## TODO: If tunneling to another country then put here the flag of the country, e.g. 🇺🇸 etc
-  echo '🌎'
-  echo '---'
- 
+if [[ $current_socks5_proxy_status == "Yes" ]] || [[ $current_http_proxy_status == "Yes" ]] || [[ $current_pac_proxy_status == "Yes" ]] ; then
+
   if [[ $current_socks5_proxy_status == "Yes" ]]; then
-    networksetup -getsocksfirewallproxy $INTERFACE
+    echo '🇬🇧'
+    # networksetup -getsocksfirewallproxy $INTERFACE
   fi
 
   if [[ $current_http_proxy_status == "Yes" ]]; then
-    networksetup -getwebproxy $INTERFACE
+    echo '🌍'
+    # networksetup -getwebproxy $INTERFACE
   fi
+
+  if [[ $current_pac_proxy_status == "Yes" ]]; then
+    echo '📡'
+    # networksetup -getautoproxyurl $INTERFACE
+  fi
+  echo '---'
+
 else
-  ## TODO: put here the emoji for your real IP country e.g. 🇪🇸 🇫🇷 🇮🇹 🇩🇪 🇨🇳 🇺🇸 🇬🇧 etc
-  echo "➖"
+  echo "🇪🇸"
   echo '---'
 fi
 
 echo '---'
 
 if [[ $current_socks5_proxy_status == "Yes" ]]; then
-  echo "🌎 SOCKS PROXY is ON! Click to stop socks5://$SOCKS_PROXY_HOST:$SOCKS_PROXY_PORT | bash='$0' color=indianred param1=disable_socks5_proxy refresh=true terminal=false"
+  echo "✅ SOCKS PROXY is ON! Click to stop socks5://$SOCKS_PROXY_HOST:$SOCKS_PROXY_PORT | bash='$0' color=indianred param1=disable_socks5_proxy refresh=true terminal=false"
 else
   echo "❌ SOCKS PROXY is OFF! Click to start socks5://$SOCKS_PROXY_HOST:$SOCKS_PROXY_PORT | bash='$0' param1=enable_socks5_proxy refresh=true terminal=false"
 fi
 
 if [[ $current_http_proxy_status == "Yes" ]]; then
-  echo "🌏 HTTP PROXY is ON! Click to stop http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT | bash='$0' color=indianred param1=disable_http_proxy refresh=true terminal=false"
+  echo "✅ HTTP PROXY is ON! Click to stop http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT | bash='$0' color=indianred param1=disable_http_proxy refresh=true terminal=false"
 else
   echo "❌ HTTP PROXY is OFF! Click to start http://$HTTP_PROXY_HOST:$HTTP_PROXY_PORT | bash='$0' param1=enable_http_proxy refresh=true terminal=false"
 fi
 
+
+if [[ $current_pac_proxy_status == "Yes" ]]; then
+  echo "✅ PAC PROXY is ON! Click to stop $PAC_PROXY | bash='$0' color=indianred param1=disable_pac_proxy refresh=true terminal=false"
+else
+  echo "❌ PAC PROXY is OFF! Click to start $PAC_PROXY | bash='$0' param1=enable_pac_proxy refresh=true terminal=false"
+fi
+
 echo '---'
-echo "📝 Edit this file | bash='$0' param1="edit_this_script" terminal=false"
+echo "✏️ Edit this file | bash='$0' param1="edit_this_script" terminal=false"
 
 echo '---'
 echo "🔃 Refresh... | refresh=true"
