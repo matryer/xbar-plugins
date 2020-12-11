26fe42f40ba4d683441f2e53175c12cb78774a9a
Robert Prince <robert.prince@anchore.com>
2020-12-11 10:01:10 -0800

    update a bit, add image

--- Dev/Kubernetes/k-get-pods.30s.sh
+++ Dev/Kubernetes/k-get-pods.30s.sh
@@ -1,16 +1,31 @@
 #!/usr/bin/env bash
 
-# <bitbar.title>List running Kubernetes pods (default namespace)</bitbar.title>
+# <bitbar.title>List some running Kubernetes things</bitbar.title>
 # <bitbar.version>v1.0</bitbar.version>
 # <bitbar.author>Robert Prince</bitbar.author>
 # <bitbar.author.github>robertp</bitbar.author.github>
-# <bitbar.desc>Simple that shows running Kubernetes pods; it's just output from 'kubectl get pods' with a font specified. It assumes you have installed kubectl using brew.</bitbar.desc>
+# <bitbar.desc>Simple plugin that shows running Kubernetes pods, services, deployments, ...</bitbar.desc>
 # <bitbar.dependencies>brew,kubectl</bitbar.dependencies>
+# <bitbar.image>https://i.imgur.com/sH9yhBW.png</bitbar.image>
 
 export PATH=/usr/local/bin:"${PATH}"
 
-numpods=$(kubectl get pods -A 2> /dev/null | grep -v NAME | wc -l)
-echo "$numpods pods running"
+numpods=$(kubectl get pods -A 2> /dev/null | grep -v NAME | wc -l | sed 's/ //g')
+numsvc=$(kubectl get services -A 2> /dev/null | grep -v NAME | wc -l | sed 's/ //g')
+numdeps=$(kubectl get deployments -A 2> /dev/null | grep -v NAME | wc -l | sed 's/ //g')
+
+# if [[ "$numpods" -eq "0" && "$numsvc" -eq "0" && "$numdeps" -eq "0" ]]; then echo "no k8s"; exit; fi
+
+if [[ "$numpods" -eq "0" && "$numsvc" -eq "0" && "$numdeps" -eq "0" ]]; then exit; fi
+
+echo "[$numpods pods / $numsvc services / $numdeps deployments]"
+
 echo "---"
-if [[ "$numpods" -eq 0 ]]; then exit; fi
+echo "==== PODS ===="
 kubectl get pods -A | while read -r line; do echo "${line} | font=Menlo"; done
+echo "---"
+echo "==== SERVICES ===="
+kubectl get services -A | while read -r line; do echo "${line} | font=Menlo"; done
+echo "---"
+echo "==== DEPLOYMENTS ===="
+kubectl get deployments -A | while read -r line; do echo "${line} | font=Menlo"; done
