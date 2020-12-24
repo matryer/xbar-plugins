480c34b6af0ee2b81ee616ff2637c8893b330efc
j0nl1 <jonlitech@gmail.com>
2020-12-24 10:06:13 +0100

    Add script to get bitcoin price (USD) in binance

---
+++ Cryptocurrency/Bitcoin/binance.com/bitcoint-usd.10s.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# <bitbar.title>Bitcoin Price</bitbar.title>
+# <bitbar.version>v1.0</bitbar.version>
+# <bitbar.author>Javier Aceña</bitbar.author>
+# <bitbar.author.github>j0nl1</bitbar.author.github>
+# <bitbar.desc>This plugin displays the current price of bitcoin in USD from Binance platform</bitbar.desc>
+# <bitbar.image>https://imgur.com/a/GVsqtJm</bitbar.image>
+#
+
+RESULT=$(curl -s "https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT")
+
+echo "$RESULT" | egrep -o '[0-9]+(\.)' | sed '$ s/\./$/'
