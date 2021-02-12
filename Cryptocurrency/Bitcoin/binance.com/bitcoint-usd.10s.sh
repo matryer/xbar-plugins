50debb910ce37f9668671b5cff8b2e7f7b428ddc
j0nl1 <jonlitech@gmail.com>
2021-02-13 00:11:23 +0100

    Update image url

--- Cryptocurrency/Bitcoin/binance.com/bitcoint-usd.10s.sh
+++ Cryptocurrency/Bitcoin/binance.com/bitcoint-usd.10s.sh
@@ -5,7 +5,7 @@
 # <bitbar.author>Javier Aceña</bitbar.author>
 # <bitbar.author.github>j0nl1</bitbar.author.github>
 # <bitbar.desc>This plugin displays the current price of bitcoin in USD from Binance platform</bitbar.desc>
-# <bitbar.image>https://imgur.com/a/GVsqtJm</bitbar.image>
+# <bitbar.image>https://i.imgur.com/npWiJRn.png/bitbar.image>
 #
 
 RESULT=$(curl -s "https://api.binance.com/api/v1/ticker/price?symbol=BTCUSDT")
