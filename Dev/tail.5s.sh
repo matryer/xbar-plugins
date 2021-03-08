b3c2629fafcf102189442484c1775024a4e6d159
matryer <thatmatryer@gmail.com>
2021-03-08 14:34:28 +0000

    no need to set the env var using xbar app

--- Dev/tail.5s.sh
+++ Dev/tail.5s.sh
@@ -4,7 +4,7 @@
 # <bitbar.version>v2.0</bitbar.version>
 # <bitbar.author>Mat Ryer</bitbar.author>
 # <bitbar.author.github>matryer</bitbar.author.github>
-# <bitbar.desc>Tails a text file, set `FILE` env var. Perfect for tailing logs in the menu bar.</bitbar.desc>
+# <bitbar.desc>Tails a text file. Perfect for tailing logs in the menu bar.</bitbar.desc>
 # <bitbar.image>https://cloud.githubusercontent.com/assets/101659/12247623/b65b6f1e-b8ac-11e5-8ec2-6d9d885bfb6f.png</bitbar.image>
 # <bitbar.dependencies></bitbar.dependencies>
 # <bitbar.abouturl>https://github.com/matryer/bitbar-plugins/blob/master/Dev/Logs/tail.5s.sh</bitbar.abouturl>
