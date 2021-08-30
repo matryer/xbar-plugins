45a8589716ee4fdde3a140a78a824417bcc11cdd
C4Wiz <c4wiz@live.com>
2021-08-30 07:50:56 -0400

    Updated for xbar v2.1.16
    Added Terminal=true
    use /bin/zsh

--- Network/ssh.sh
+++ Network/ssh.sh
@@ -1,10 +1,10 @@
-#!/usr/bin/env bash
+#!/bin/zsh
 #
 # Quickly SSH to your favorite hosts
 # The list of hosts are extracted from ~/.ssh/config
 #
 # <xbar.title>SSH</xbar.title>
-# <xbar.version>v1.0</xbar.version>
+# <xbar.version>v1.1</xbar.version>
 # <xbar.author>Thameera Senanayaka</xbar.author>
 # <xbar.author.github>thameera</xbar.author.github>
 # <xbar.desc>Quickly SSH to your favorite hosts listed in your ~/.ssh/config file</xbar.desc>
@@ -12,4 +12,4 @@
 
 echo "ssh"
 echo "---"
-awk  '/^Host / && !/\*/ {print $2" | bash=ssh param1="$2}' ~/.ssh/config
+awk '/^Host / && !/\*/ {print $2" | shell=ssh param1="$2 " terminal=true"}' ~/.ssh/config
