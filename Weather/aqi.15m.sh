f68729c4406eb294185d113ea713883daa5c2bf1
Nick Xiao <nick.xiao@zoom.us>
2021-12-17 13:35:07 +0800

    minor tweaks

--- Weather/aqi.15m.sh
+++ Weather/aqi.15m.sh
@@ -8,22 +8,23 @@
 # <xbar.image>https://github.com/nicoster/assets/blob/fe2a768dc41ad7e4b164260346b5f0b461b8ba87/xbar-aqi.png?raw=true</xbar.image>
 # <xbar.dependencies>bash</xbar.dependencies>
 # <xbar.abouturl>http://www.yuanchongyu.com</xbar.abouturl>
-# <xbar.var>string(URL="https://aqicn.org/city/usa/newyork/"): Navigate to `https://aqicn.org/`, find your city or nearest location, then copy from the address bar of the browser and paste it below.</xbar.var>
+# <xbar.var>string(URL="https://aqicn.org/city/usa/newyork/?demo=1"): Navigate to `https://aqicn.org/`, find your city or nearest location, then copy from the address bar of the browser and paste it here.</xbar.var>
 
 
 # URL examples
 # URL="https://aqicn.org/city/usa/newyork/queens-college"
 # URL="https://aqicn.org/city/california/santa-clara/san-jose-jackson-st/"
-URL="https://aqicn.org/city/usa/newyork/?demo"
+# URL="https://aqicn.org/city/usa/newyork/?demo=1"
+
 
 MENUFONT="size=12 font=UbuntuMono-Bold"
 COLORS=("#0ed812" "#ffde33" "#ff9933" "#cc0033" "#660099" "#7e0023" "#404040")
 EMOJIS=("😀" "🙁" "😨" "😷" "🤢" "💀" "☠️")
 
 TYPES=("pm25" "pm10" "o3" "no2" "so2" "co" "t" "p" "h" "w")
-TITLES=("[PM25]" "[PM10]" "[  O3]" "[ NO2]" "[ SO2]" "[  CO]" "[TEMP]" "[PRES]" "[HUMI]" "[WIND]")
+TITLES=("PM25" "PM10" "  O3" " NO2" " SO2" "  CO" "TEMP" "PRES" "HUMI" "WIND")
 
-function extract_by_id {
+function val_by_id {
   echo "$1" | sed -n -E "s/^.*id='$2'[^>]*>(<[^>]+>)?([^<]*).*$/\2/p"
 }
 
@@ -65,7 +66,7 @@ function emoji {
 
 HTML=$(curl -s $URL)
 IMGS=$(echo "${HTML}" | grep -o -E "<img class='aqi-graph-img[^:]+:image/png;base64,[^']*" | sed -n -E "s/^.*base64,([^']*).*$/\1/p")
-AQI=$(extract_by_id "${HTML}" "aqiwgtvalue")
+AQI=$(val_by_id "${HTML}" "aqiwgtvalue")
 
 COLOR="$(colorize "${AQI}")"
 EMOJI="$(emoji "${AQI}")"
@@ -73,23 +74,23 @@ echo "${EMOJI} ${AQI} | color=${COLOR} ${MENUFONT}"
 
 echo "---"
 echo "Sync with ${URL} | refresh=true"
+echo "$(val_by_id "${HTML}" "aqiwgtutime") | size=10"
 if [[ ${URL} =~ .*"demo" ]]; then
   echo "Press ⌘ E to configure your location"
 fi
 
-echo "─────────────────────────────────────── min-cur-max | font=Monaco size=12"
-
+echo "──────────────────────────────────────────── min-cur-max | font=Monaco size=10"
 i=0
 for img in ${IMGS} ;do
-  MIN=$(extract_by_id "${HTML}" "min_${TYPES[$i]}")
+  MIN=$(val_by_id "${HTML}" "min_${TYPES[$i]}")
   while [[ "${MIN}" == "" ]]; do
     i=$((i+1))
-    MIN=$(extract_by_id "${HTML}" "min_${TYPES[$i]}")
+    MIN=$(val_by_id "${HTML}" "min_${TYPES[$i]}")
   done
 
-  printf "%s\t" "${TITLES[$i]}" 
-  echo "${MIN}-$(extract_by_id "${HTML}" "cur_${TYPES[$i]}")-$(extract_by_id "${HTML}" "max_${TYPES[$i]}") | href=${URL} font=Monaco size=12 image=${img} " #
-  i=$((i+1))
+  printf "%s " "${TITLES[$i]}" 
+  echo "${MIN}-$(val_by_id "${HTML}" "cur_${TYPES[$i]}")-$(val_by_id "${HTML}" "max_${TYPES[$i]}") | href=${URL} font=Monaco size=10 image=${img} trim=false" #
+  i=$((i+1)) 
 done
 
 echo "Visit World Air Quality Index site to support the team | href=${URL} color=blue"
