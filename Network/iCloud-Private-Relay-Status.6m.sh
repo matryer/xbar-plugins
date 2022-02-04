b028dd09c0b56005d1d4e7d43f69b4ed086b3cb4
Adam Rice <sudama@gmail.com>
2022-02-04 12:49:33 -0500

    Adding region reporting
    Now we look up the region in Apple's egress list and report it in the menu bar and in the menu. I might take it out of the menu bar again, cause space is at a premium for me!

--- Network/iCloud-Private-Relay-Status.6m.sh
+++ Network/iCloud-Private-Relay-Status.6m.sh
@@ -16,9 +16,6 @@ if [ $(curl -LI http://google.com -o /dev/null -w '%{http_code}\n' -s) == "200" 
     iCloudPrivateRelayIP=$(curl -s http://ifconfig.me)
     # using http and ifconfig.me seems to reliably return the iCloud Private Relay IP if Private Relay is enabled
 
-    # build the strings to use for the menu output
-    wanresult_string="Public IP: $wanIP | bash='/bin/bash' param1='-c' param2='/bin/echo $wanIP | pbcopy' terminal=false"
-    icprresult_string="iCloud Relay IP: $iCloudPrivateRelayIP | bash='/bin/bash' param1='-c' param2='/bin/echo $iCloudPrivateRelayIP | pbcopy' terminal=false"
 
 
     # now, get the public list of iCloud Private Relay egresses
@@ -33,15 +30,19 @@ if [ $(curl -LI http://google.com -o /dev/null -w '%{http_code}\n' -s) == "200" 
     # get a new copy of the egress ips every few days rather than every run
     if [ ! -f "$iCloudEgressList" ]; then
         curl --location --silent "https://mask-api.icloud.com/egress-ip-ranges.csv" -o "$iCloudEgressList"         
-    elif [[ $(find "$iCloudEgressList" -mtime +3 -print) ]]; then # it's older than 3 days; delete and get a new one
+    elif [[ $(find "$iCloudEgressList" -mtime +3 -print) ]]; then # it's older than 7 days; delete and get a new one
         rm iCloudEgressList
         curl --location --silent "https://mask-api.icloud.com/egress-ip-ranges.csv" -o "$iCloudEgressList"         
     fi
-  
+   
+    ICPResult_in_EgressList=$(fgrep "$iCloudPrivateRelayIP" "$iCloudEgressList")
     # test if our potential relay IP shows up in the list we just got from Apple
-    ICPResult_in_EgressList=$(grep "$iCloudPrivateRelayIP" "$iCloudEgressList")
-    
-    
+    if [ ICPResult_in_EgressList != "" ]; then
+        Region=$(echo $ICPResult_in_EgressList | awk -F, '{print $4}')
+        if [ "$Region" = "" ]; then
+            Region="Region unlisted"
+        fi
+    fi
     # here, we set the menu bar icon to either a green sparkle (yay safe!) or a red flag (boo danger!)
     if [ "$wanIP" != "$iCloudPrivateRelayIP" ] && [ ICPResult_in_EgressList != "" ]; then
         ICP_active=true
@@ -54,6 +55,11 @@ else
     wanresult_string="No Internet Connection"
     echo "❌️"
 fi
+
+    # build the strings to use for the menu output
+    wanresult_string="Public IP: $wanIP | bash='/bin/bash' param1='-c' param2='/bin/echo $wanIP | pbcopy' terminal=false"
+    icprresult_string="iCloud Relay IP: $iCloudPrivateRelayIP ($Region) | bash='/bin/bash' param1='-c' param2='/bin/echo $iCloudPrivateRelayIP | pbcopy' terminal=false"
+
 
 d=$(date +"%A, %b %d, %I:%M %p")
 
@@ -69,9 +75,10 @@ fi
 
 # menu items for screenshot
 # echo "Public IP: 25.0.0.7"
-# echo "iCloud Relay IP: 172.224.224.7"
-
+# echo "iCloud Relay IP: 172.224.224.7 (London)"
+# echo "($Region)"
 echo "Checked: $d"
-
+echo "Apple ID Preferences... | bash='/bin/bash' param1='-c' param2='/usr/bin/open /System/Library/PreferencePanes/AppleIDPrefPane.prefPane/' terminal=false"
+# echo $ICPResult_in_EgressList
 echo ---
 echo "Refresh... | refresh=true"
