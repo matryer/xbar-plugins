0386e560c5cc4e1aa5a7a2eaa6f4cc36fc8da149
tadamcz <tadamczewskipublic@gmail.com>
2023-04-06 14:10:37 +0100

    remove requests dependency

--- Web/beeminder_one_goal.1m.py
+++ Web/beeminder_one_goal.1m.py
@@ -1,4 +1,4 @@
-#! ../python_env/bin/python
+#!/usr/bin/env -S PATH="${PATH}:/opt/homebrew/bin:/usr/local/bin" python3
 # -*- coding: utf-8 -*-
 
 # Metadata allows the plugin to show up in the xbar app and website.
@@ -9,16 +9,18 @@
 #  <xbar.author.github>tadamcz</xbar.author.github>
 #  <xbar.desc>Concisely show the status of a Beeminder goal</xbar.desc>
 #  <xbar.image>https://images2.imgbox.com/8d/2d/wLOcD9sz_o.png</xbar.image>
-#  <xbar.dependencies>python,python requests library</xbar.dependencies>
+#  <xbar.dependencies>python</xbar.dependencies>
 
 # <xbar.var>string(VAR_USERNAME='johnsmith'): Your Beeminder username</xbar.var>
 # <xbar.var>string(VAR_AUTH_TOKEN=''): Your Beeminder auth token</xbar.var>
 # <xbar.var>string(VAR_GOAL_SLUG='exercise'): Your Beeminder goal slug e.g. 'exercise' for beeminder.com/myusername/exercise</xbar.var>
 # <xbar.var>string(VAR_GOAL_EMOJI='🏋️'): Emoji to represent your goal (will appear in the menu bar)</xbar.var>
 
-import requests
-from requests.adapters import HTTPAdapter, Retry
 import os
+import json
+from urllib.request import urlopen, Request
+from urllib.parse import urlencode
+from urllib.error import URLError
 
 # Script-level variables
 USERNAME = os.environ["VAR_USERNAME"]
@@ -26,23 +28,18 @@ AUTH_TOKEN = os.environ["VAR_AUTH_TOKEN"]
 GOAL_SLUG = os.environ["VAR_GOAL_SLUG"]
 GOAL_EMOJI = os.environ["VAR_GOAL_EMOJI"]
 
-# Set up Session with retries
-retries = Retry(total=10, backoff_factor=0.1)
-s = requests.Session()
-s.mount('https://', HTTPAdapter(max_retries=retries))
-
 # Get data
-if not USERNAME or not AUTH_TOKEN:
-    print('⚠️\n---\nBeeminder: No username and/or auth token provided!')
+API_URL = f'https://www.beeminder.com/api/v1/users/{USERNAME}.json'
+params = dict(auth_token=AUTH_TOKEN, datapoints_count=1, associations=True)
+url_with_params = f'{API_URL}?{urlencode(params)}'
+
+try:
+    response = urlopen(url_with_params)
+    data = json.loads(response.read())
+    goals = data['goals']
+except URLError as e:
+    print(f'Error fetching data: {e}')
     exit(1)
-API_URL = 'https://www.beeminder.com/api/v1/users/{}.json'.format(USERNAME)
-
-
-req = s.get(API_URL, params=dict(
-    auth_token=AUTH_TOKEN, datapoints_count=1, associations=True))
-
-data = req.json()
-goals = data['goals']
 
 # Select goal
 goal_emoji = GOAL_EMOJI
@@ -52,21 +49,21 @@ goal = chinups
 
 # Assemble output
 color_emojis = {
-    "green":"🟢",
-    "blue":"🔵",
+    "green": "🟢",
+    "blue": "🔵",
     "orange": "🟠",
-    "red":"🔴",
+    "red": "🔴",
 }
 
-goal_url = 'https://www.beeminder.com/{}/{}'.format(USERNAME, goal['slug'])
+goal_url = f'https://www.beeminder.com/{USERNAME}/{goal["slug"]}'
 color_emoji = color_emojis[goal["roadstatuscolor"]]
 message = goal['limsumdays']
 
 # Abbreviate message to take up less space in menu bar
-message = message.replace("due ", "").replace("days","d").replace("day","d")
+message = message.replace("due ", "").replace("days", "d").replace("day", "d")
 
 output = []
-output.append(f"{goal_emoji}{color_emoji}{message}") # Shown in menu bar
+output.append(f"{goal_emoji}{color_emoji}{message}")  # Shown in menu bar
 output.append("---")
-output.append(f"{goal_url} | href={goal_url}") # Shown in dropdown
+output.append(f"{goal_url} | href={goal_url}")  # Shown in dropdown
 print("\n".join(output))
