3780e2b2c05b9507781eb36370d67c50615b92c9
Luis Cruz <sprak3000+github@gmail.com>
2023-05-08 10:19:28 -0400

    Update network-latency.5s.py
    - Adding image metadata tag.
    - Convert plugin to use xbar variables for target and target name settings.
    - Fixing an issue where the blank color breaks the output.
    - Removing `sfcolor` as that breaks the output. Might break swiftbar compatibility. 🤷🏽

--- Network/network-latency.5s.py
+++ Network/network-latency.5s.py
@@ -1,18 +1,21 @@
-#! /usr/bin/python3
+#!/usr/bin/env python3
 
 # <xbar.title>Display network latency</xbar.title>
 # <xbar.version>v1.1</xbar.version>
 # <xbar.author>Ciaran Finnegan</xbar.author>
 # <xbar.author.github>slartibastfast</xbar.author.github>
 # <xbar.desc>Display current network round trip time (latency) for ICMP to a defined target</xbar.desc>
+# <xbar.image>https://user-images.githubusercontent.com/5545555/236847602-7963f85c-9579-447d-bd1d-b07ea2dbedcc.png</xbar.image>
+# <xbar.var>string(VAR_TARGET="syd1.speedtest.telstra.net"): The fully qualified domain name or IP address to check your latency against</xbar.var>
+# <xbar.var>string(VAR_TARGET_NAME="Sydney"): The target name you wish to appear in the menu bar</xbar.var>
 # <swiftbar.hideRunInTerminal>true</swiftbar.hideRunInTerminal>
 
 import subprocess
 import re
 newline = '\n'
 
-target = 'syd1.speedtest.telstra.net' # replace with your target fully qualified domain name or IP address
-targetName ='Sydney' # replace with the target name you wish to appear in the Menu bar
+target = os.environ.get("VAR_TARGET")
+targetName = os.environ.get("VAR_TARGET_NAME")
 
 
 result = subprocess.run(['/sbin/ping','-c','5','-W','1000',target], capture_output=True, text=True).stdout
@@ -25,12 +28,12 @@ rttMax = str(round(float(resultMatch.group(4))))
 jitter = str(round(float(resultMatch.group(5))))
 
 
-if int(rttAvg) <= 300 : color = ''
+if int(rttAvg) <= 300 : color = 'green'
 if int(rttAvg) in range(300,500,1) : color = 'orange'
 if int(rttAvg) >500 : color = 'red'
 
 
-theOutput = (f':bolt.fill: {targetName} {rttAvg} ms | color={color} sfcolor={color}'
+theOutput = (f':bolt.fill: {targetName} {rttAvg} ms | color={color}{newline}'
              f'---{newline}'
              f'Ping target: {target}'
              f'{newline}Packet Loss {loss}%'
