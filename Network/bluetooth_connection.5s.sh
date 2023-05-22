2caacf3b8a781facd6c7a82af5d0dd70ecbbe42f
Luis Cruz <sprak3000+github@gmail.com>
2023-05-22 10:03:02 -0400

    Update bluetooth_connection.5s.sh
    Adding metadata.

--- Network/bluetooth_connection.5s.sh
+++ Network/bluetooth_connection.5s.sh
@@ -1,5 +1,14 @@
 #!/bin/zsh
 
+# <xbar.title>Bluetooth Connection Indicator</xbar.title>
+# <xbar.version>v1.0.0</xbar.version>
+# <xbar.author>Simon Gray</xbar.author>
+# <xbar.author.github>simongray</xbar.author.github>
+# <xbar.desc>Indicate in the menu bar if a bluetooth device is connected.</xbar.desc>
+# <xbar.image>https://github-production-user-asset-6210df.s3.amazonaws.com/5545555/239931189-6f427aec-b9c4-4ab0-b442-7a16050431cb.png</xbar.image>
+# <xbar.dependencies>blueutil</xbar.dependencies>
+# <xbar.abouturl>https://github.com/simongray</xbar.abouturl>
+
 # Note: requires blueutil
 # Install with `brew install blueutil`
 
