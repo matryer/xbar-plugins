c51b8b437a8fd9a6ffa2faab836f148d4bda4b84
Ale Narvaja <anarvaja@gmail.com>
2023-09-27 17:26:12 +0200

    feat(MicToggle): mute / unmute system mic

---
+++ System/MicToggle.1s.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# <xbar.title>Mic Toggle</xbar.title>
+# <xbar.version>v1.0</xbar.version>
+# <xbar.author>Alejandro Narvaja</xbar.author>
+# <xbar.author.github>alebarbaja</xbar.author.github>
+# <xbar.desc>Toggle microphone on/off and display its status.</xbar.desc>
+# <xbar.image>https://imgur.com/a/dBhPg2g</xbar.image>
+# <xbar.dependencies>osascript</xbar.dependencies>
+
+# Get the current input volume
+current_volume=$(osascript -e "input volume of (get volume settings)")
+
+# If the volume is 0, the mic is muted
+if [ "$current_volume" == "0" ]; then
+  echo "🔇"
+  echo "---"
+  echo "Activate Mic | bash='$0' param1=unmute terminal=false"
+else
+  echo "🔊"
+  echo "---"
+  echo "Mute Mic | bash='$0' param1=mute terminal=false"
+fi
+
+# Handle user click
+if [ "$1" == "mute" ]; then
+  osascript -e "set volume input volume 0"
+elif [ "$1" == "unmute" ]; then
+  osascript -e "set volume input volume 100"
+fi
