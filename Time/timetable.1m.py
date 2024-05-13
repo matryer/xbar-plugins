1eabb9827b1fab100a7556c3cb9217f6029aeb20
Cabin Zhu <60537086+cabinz@users.noreply.github.com>
2024-05-14 03:16:58 +0800

    [New] Timetable 2.0
    Support emoji and more configurable features.

--- Time/timetable.1m.py
+++ Time/timetable.1m.py
@@ -2,21 +2,36 @@
 
 # xbar meta
 # <xbar.title>Timetable</xbar.title>
-# <xbar.version>v1.0</xbar.version>
+# <xbar.version>v2.0</xbar.version>
 # <xbar.author>Cabin Zhu</xbar.author>
 # <xbar.author.github>cabinz</xbar.author.github>
-# <xbar.desc>Display the current event from a given daily timetable. To make it work, make sure to configure the path to your CSV timetable file, where each line is in the format of <Begin_Time,End_Time,Event_Name></xbar.desc>
-# <xbar.image>https://raw.githubusercontent.com/cabinz/my-xbar-plugins/main/timetable/timetable.png</xbar.image>
+# <xbar.desc>Display the current event from a given daily timetable. To make it work, make sure to configure the path to your CSV timetable file. More about the user manual, see project page https://github.com/cabinz/xbar-timetable.</xbar.desc>
+# <xbar.image>https://raw.githubusercontent.com/cabinz/xbar-timetable/main/docs/timetable.png</xbar.image>
+# <xbar.abouturl>https://github.com/cabinz/xbar-timetable</xbar.abouturl>
 # <xbar.dependencies>python</xbar.dependencies>
 # <xbar.var>string(VAR_TIMETABLE_FILE="/path/to/your/timetable.csv"): An absolute path to a CSV file of the timetable to be displayed.</xbar.var>
+# <xbar.var>boolean(VAR_DISPLAY_EMOJI_WHEN_GIVEN=true): Display in the menu bar the emoji instaad when given.</xbar.var>
+# <xbar.var>boolean(VAR_DISPLAY_TIME_LEFT=true): Display in the menu bar the remaining time of the current event. Otherwise it will be in the pop-up menu.</xbar.var>
+# <xbar.var>string(VAR_FONT='Monaco'): Display font in the pop-up menu.</xbar.var>
+# <xbar.var>number(VAR_FONT_SIZE=15): Display font size in the pop-up menu.</xbar.var>
 
 import datetime
 import csv
 import os
+import sys
 from typing import List
 
 
+# If you are on SwiftBar or other platforms w/o user-configurable variables,
+# manually change the assignment as a string of absolute path to your timetable CSV file.
 CSV_TIMETAB = os.environ.get("VAR_TIMETABLE_FILE")
+DISP_EMJ = os.environ.get("VAR_DISPLAY_EMOJI_WHEN_GIVEN", 'true') == 'true'
+DISP_TIME_LEFT = os.environ.get("VAR_DISPLAY_TIME_LEFT", 'true') == 'true'
+FONT = os.environ.get("VAR_FONT", 'Monaco')
+FONT_SIZ = int(os.environ.get("VAR_FONT_SIZE", '15'))
+
+RGB_LIGHT_GREY = "#848481" 
+RGB_ORANGE = "#F2980B"
 
 
 def to_m_time(time: str):
@@ -31,16 +46,27 @@ def to_timestamp(m_time: int):
     return f"{hr:02d}:{minute:02d}"
 
 
+def exit_with_error(err_msg: str):
+    print('⚠️Error')
+    print('---')
+    print(err_msg)
+    sys.exit()
+
+
 class Event:
-    def __init__(self, start_timestamp: str, end_timestamp: str, name: str) -> None:
+    def __init__(self, start_timestamp: str, end_timestamp: str, name: str, 
+                 emoji: str = None) -> None:
         self.start_time = start_timestamp
         self.end_time = end_timestamp
         self.m_start_time = to_m_time(start_timestamp)
         self.m_end_time = to_m_time(end_timestamp)
         self.name = name
+        self.emoji = emoji
     
     def __repr__(self) -> str:
-        return self.name
+        return "{}{}".format(
+            self.name, 
+            self.emoji if self.emoji else '')
     
     def spans_midnight(self) -> bool:
         return self.m_start_time > self.m_end_time
@@ -61,26 +87,39 @@ class Event:
         elif self.m_start_time <= m_time < self.m_end_time:
             return True
         return False
-        
-    def time_left(self, m_time: int) -> str:
-        m_left = self.minutes_left(m_time)
-        hr_left = m_left / 60
-        if hr_left >= 1:
-            return f"{hr_left:.1f}h"
-        else:
-            return f"{m_left}m"
 
 
 def load_timetable(csv_file) -> List[Event]:
+    """Load timetable from CSV file as a list of Events.
+    
+    Each line of the CSV file is in the format of
+    ```
+    <start_time>,<end_time>,<event_name>[,<event_emoji>]
+    ```    
+    The event emoji column is optional.
+    """
     if csv_file is None or not os.path.exists(csv_file):
-        raise ValueError(
-            f'The current timetable file path "{csv_file}" is invalid. Please re-configure the path to your CSV file in xbar. If you are a user from SwiftBar or other plug-in software that does not support user-configurable variables, you could modify the script to hard-code the path yourself as the CSV_TIMETAB variable.')
+        exit_with_error(
+            f'Invalid timetable file path: {csv_file}\n'
+            'Please configure the absolute path to your CSV file in xbar.\n'
+            'If you are on SwiftBar or other platforms w/o user-configurable variables,\n'
+            'modify the plugin script yourself to specify the path as the variable CSV_TIMETAB.')
     
     tab = []
     with open(csv_file, newline='') as file:
         reader = csv.reader(file)
-        for idx, (start_time, end_time, event_name) in enumerate(reader):
-            tab.append(Event(start_time, end_time, event_name))
+        for idx, ln in enumerate(reader):
+            if len(ln) not in (3, 4):
+                exit_with_error(
+                    'Each line of the CSV file needs to have 3 or 4 columns,\n'
+                    f'but line:{idx} (start from 0) in the given file has {len(ln)} columns.')
+            if len(ln) == 3:
+                ln.append(None)
+            elif ln[3] == '':
+                ln[3] = None
+                
+            start_time, end_time, event_name, event_emoji = ln
+            tab.append(Event(start_time, end_time, event_name, emoji=event_emoji))
     return tab
 
 
@@ -96,6 +135,15 @@ def locate_event(cur_m_time: int, timetable: List[Event]) -> int:
     return -1
 
 
+def get_time_left_str(m_left: int) -> str:
+    hr_left = m_left / 60
+    if hr_left >= 1:
+        t = f"{hr_left:.1f}h"
+    else:
+        t = f"{m_left}m"
+    return f"({t} left)"
+
+
 if __name__ == "__main__":
     table = load_timetable(CSV_TIMETAB)
     cur_time = datetime.datetime.now().strftime("%H:%M")
@@ -104,18 +152,29 @@ if __name__ == "__main__":
     idx_found = locate_event(cur_m_time, table)
     if idx_found != -1: # display the first-hit ongoing event as title
         event = table[idx_found]
-        print(f'{event} ({event.time_left(cur_m_time)} left)')
-        # print(f'{event} | dropdown=False')
-        # print(f'{event.time_left(cur_m_time)} left')
+        print('{} {}'.format(
+            event.emoji if DISP_EMJ and event.emoji else event.name,
+            get_time_left_str(event.minutes_left(cur_m_time)) if DISP_TIME_LEFT else ""
+        ))
     else:
         print("no event")
     
     print("---")
-    RGB_LIGHT_GREY = "#848481" 
-    for cur_idx, event in enumerate(table):
-        is_ongoing = event.is_ongoing(cur_m_time) # allows multiple ongoing events
-        print("{}-{} {}{} | font=Monaco size=15 color={}".format(
-            event.start_time, event.end_time, event.name,
-            " ⬅" if is_ongoing else "",
-            "orange" if is_ongoing else RGB_LIGHT_GREY
+    if idx_found != -1 and not DISP_TIME_LEFT:
+        print('{} | font={} size={} color={}'.format(
+            get_time_left_str(event.minutes_left(cur_m_time)),
+            FONT, FONT_SIZ, RGB_ORANGE
         ))
+    for cur_idx, event in enumerate(table):
+        if event.is_ongoing(cur_m_time): # allows multiple ongoing events
+            print("{}-{}  {} {}| font={} size={} color={}".format(
+                event.start_time, event.end_time, 
+                event.name, 
+                event.emoji if event.emoji else "⬅",
+                FONT, FONT_SIZ, RGB_ORANGE
+            ))
+        else:
+            print("{}-{}  {}| font={} size={} color={}".format(
+                event.start_time, event.end_time, event.name, 
+                FONT, FONT_SIZ, RGB_LIGHT_GREY
+            ))
