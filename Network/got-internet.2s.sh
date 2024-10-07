f96fcab80822a75f52aa3ea74bbce5b0622dc993
fregante <me@fregante.com>
2024-10-08 00:30:46 +0700

    Update author (#2063)

--- Network/got-internet.2s.sh
+++ Network/got-internet.2s.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # <xbar.title>Got Internet?</xbar.title>
 # <xbar.version>v1.0</xbar.version>
-# <xbar.author>Federico Brigante</xbar.author>
-# <xbar.author.github>bfred-it</xbar.author.github>
+# <xbar.author>Fregante</xbar.author>
+# <xbar.author.github>fregante</xbar.author.github>
 # <xbar.desc>Checks the connection to Internet and tells you in a single character.</xbar.desc>
 # <xbar.image>http://i.imgur.com/I8lF8st.png</xbar.image>
 
