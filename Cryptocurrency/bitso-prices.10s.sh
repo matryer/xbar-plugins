efdedf95eb26fdaa15190caac64352e3da829811
Alex Ventura <me@alexventura.dev>
2025-03-24 15:51:34 -0600

    Follow the same order in the cryptos.

--- Cryptocurrency/bitso-prices.10s.sh
+++ Cryptocurrency/bitso-prices.10s.sh
@@ -22,40 +22,40 @@ SHOW_PRICES_IN_USD="true"
 
 # Fetch data
 BTC_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=btc_mxn')
+LTC_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=ltc_mxn')
 ETH_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=eth_mxn')
 XRP_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=xrp_mxn')
 BCH_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=bch_mxn')
-LTC_JSON_MXN=$(curl -s 'https://api.bitso.com/v3/ticker/?book=ltc_mxn')
 
 BTC_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=btc_usd')
+LTC_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=ltc_usd')
 ETH_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=eth_usd')
 XRP_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=xrp_usd')
 BCH_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=bch_usd')
-LTC_JSON_USD=$(curl -s 'https://api.bitso.com/v3/ticker/?book=ltc_usd')
 
 # Select currency
 if [[ "$SHOW_PRICES_IN_USD" == "true" ]]; then
   CURRENCY="USD"
   BTC_JSON_DATA="$BTC_JSON_USD"
+  LTC_JSON_DATA="$LTC_JSON_USD"
   ETH_JSON_DATA="$ETH_JSON_USD"
   XRP_JSON_DATA="$XRP_JSON_USD"
   BCH_JSON_DATA="$BCH_JSON_USD"
-  LTC_JSON_DATA="$LTC_JSON_USD"
 else
   CURRENCY="MXN"
   BTC_JSON_DATA="$BTC_JSON_MXN"
+  LTC_JSON_DATA="$LTC_JSON_MXN"
   ETH_JSON_DATA="$ETH_JSON_MXN"
   XRP_JSON_DATA="$XRP_JSON_MXN"
   BCH_JSON_DATA="$BCH_JSON_MXN"
-  LTC_JSON_DATA="$LTC_JSON_MXN"
 fi
 
 # Extract last prices
 BTC_NOW=$(echo "$BTC_JSON_DATA" | jq -r '.payload.last')
+LTC_NOW=$(echo "$LTC_JSON_DATA" | jq -r '.payload.last')
 ETH_NOW=$(echo "$ETH_JSON_DATA" | jq -r '.payload.last')
 XRP_NOW=$(echo "$XRP_JSON_DATA" | jq -r '.payload.last')
 BCH_NOW=$(echo "$BCH_JSON_DATA" | jq -r '.payload.last')
-LTC_NOW=$(echo "$LTC_JSON_DATA" | jq -r '.payload.last')
 
 # Display prices
 printf "BTC: %'.0f %s | color=white \n" "$BTC_NOW" "$CURRENCY"
