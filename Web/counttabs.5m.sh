bbcfca4a1577c515865afe2fa55d7db633c35221
Tom Matthews <trcm@pm.me>
2026-06-02 16:59:49 +0100

    Update for counting both Safari and Chrome tabs

--- Web/counttabs.5m.sh
+++ Web/counttabs.5m.sh
@@ -1,59 +1,120 @@
 #!/usr/bin/env bash
-#  <xbar.title>chrome tabs counter</xbar.title>
-#  <xbar.version>v1.0</xbar.version>
-#  <xbar.author>Tom Matthews</xbar.author>
-#  <xbar.author.github>tomtastic</xbar.author.github>
-#  <xbar.desc>Counts the number of open Chrome tabs.</xbar.desc>
-#  <xbar.image>https://www.dropbox.com/s/0scul4sv8jtw4ev/counttabs.jpg?raw=1</xbar.image>
-#  <xbar.dependencies>osascript</xbar.dependencies>
+# <xbar.title>Tabs Counter</xbar.title>
+# <xbar.version>v2.0</xbar.version>
+# <xbar.author>Tom Matthews</xbar.author>
+# <xbar.author.github>tomtastic</xbar.author.github>
+# <xbar.desc>Counts the number of open Safari and/or Chrome tabs, and the memory used by both</xbar.desc>
+# <xbar.image>https://www.dropbox.com/s/0scul4sv8jtw4ev/counttabs.jpg?raw=1</xbar.image>
+# <xbar.dependencies>osascript</xbar.dependencies>
 
-# Number of tabs open, and the accompanying message color...
+set -euo pipefail
+
 acceptable=10
-acceptable_color="#2074D5" #sky
+acceptable_color="#2074D5" # sky
 worrying=25
-worrying_color="#FF8500" #smile
-alarming_color="#DF2A5D" #cosmos
+worrying_color="#FF8500" # smile
+alarming_color="#DF2A5D" # cosmos
 
-script='
-if application "Google Chrome" is running then
-    tell application "Google Chrome"
-        set windowlist to every window
-        set tabcount to 0
-        set totaltabcount to 0
-        set results to ""
-        repeat with chromewindow in windowlist
-            try
-                set tabcount to number of tabs in chromewindow
-                set totaltabcount to totaltabcount + tabcount
-            on error errmsg
-                -- log "error message: " & errmsg
-            end try
-            set results to results & tabcount & " "
-        end repeat
-        set results to results & totaltabcount
-        log results
-    end tell
-else
-    log "0"
-end if
-'
+get_tab_counts() {
+    local app_name=$1
 
-# Lets pop the results in an array
-read -r -a output <<< "$(/usr/bin/osascript -e "$script" 2>&1)"
+    /usr/bin/osascript -e "if application \"$app_name\" is running then
+        tell application \"$app_name\"
+            set windowlist to every window
+            set tabcount to 0
+            set totaltabcount to 0
+            set results to \"\"
+            repeat with browserwindow in windowlist
+                try
+                    set tabcount to number of tabs in browserwindow
+                    set totaltabcount to totaltabcount + tabcount
+                on error errmsg
+                    set tabcount to 0
+                end try
+                set results to results & tabcount & \" \"
+            end repeat
+            set results to results & totaltabcount
+            return results
+        end tell
+    else
+        return \"0\"
+    end if" 2>/dev/null || echo "0"
+}
 
-num_windows=${#output[@]}-1
+get_mem_size() {
+    local process_name=$1
+    local rss
 
-# Last item has total tabs value
-if [[ "${output[$num_windows]}" -le $acceptable ]]; then
+    rss=$({ ps -eo rss,comm 2>/dev/null || true; } | awk -v process_name="$process_name" '
+        index($0, process_name) {
+            total += $1
+        }
+        END {
+            printf "%.0f", total / 1024
+        }
+    ')
+
+    echo "${rss:-0}"
+}
+
+browser_running() {
+    local app_name=$1
+    [[ "$(/usr/bin/osascript -e "application \"$app_name\" is running" 2>/dev/null || true)" == "true" ]]
+}
+
+print_window_rows() {
+    local app_name=$1
+    local process_name=$2
+    shift 2
+    local counts=("$@")
+    local num_windows=${#counts[@]}
+
+    for ((i = 0; i < num_windows; i++)); do
+        echo "🪟 $((i + 1)) - has ${counts[$i]} tabs | shell=/usr/bin/osascript param1='-e' param2='tell Application \"System Events\" to tell process \"$process_name\" to perform action \"AXRaise\" of window $((i + 1))' param3='-e' param4='tell Application \"$app_name\" to activate'"
+    done
+}
+
+read -r -a safari_output <<< "$(get_tab_counts "Safari")"
+read -r -a chrome_output <<< "$(get_tab_counts "Google Chrome")"
+
+safari_total_index=$((${#safari_output[@]} - 1))
+chrome_total_index=$((${#chrome_output[@]} - 1))
+safari_total=${safari_output[$safari_total_index]}
+chrome_total=${chrome_output[$chrome_total_index]}
+safari_windows=("${safari_output[@]:0:$safari_total_index}")
+chrome_windows=("${chrome_output[@]:0:$chrome_total_index}")
+safari_mem=$(get_mem_size "Safari.app")
+chrome_mem=$(get_mem_size "Google Chrome.app")
+total_tabs=$((safari_total + chrome_total))
+total_mem=$((safari_mem + chrome_mem))
+
+if [[ "$total_tabs" -le $acceptable ]]; then
     color=$acceptable_color
-elif [[ "${output[$num_windows]}" -le $worrying ]]; then
+elif [[ "$total_tabs" -le $worrying ]]; then
     color=$worrying_color
 else
     color=$alarming_color
 fi
 
-echo "🐿 ${output[$num_windows]} Chrome tabs open | color=$color"
+echo "🐿 $total_tabs tabs | color=$color"
 echo "---"
-for (( i=0; i<num_windows; i++ )); do
-    echo "🪟 $(( i+1 )) - has ${output[$i]} tabs"
-done
+echo "Safari: $safari_total tabs, ${#safari_windows[@]} windows"
+echo "Chrome: $chrome_total tabs, ${#chrome_windows[@]} windows"
+
+if browser_running "Safari" && ((${#safari_windows[@]} > 0)); then
+    echo "---"
+    echo "Safari"
+    print_window_rows "Safari" "Safari" "${safari_windows[@]}"
+fi
+
+if browser_running "Google Chrome" && ((${#chrome_windows[@]} > 0)); then
+    echo "---"
+    echo "Chrome"
+    print_window_rows "Google Chrome" "Google Chrome" "${chrome_windows[@]}"
+fi
+
+echo "---"
+echo "Memory"
+echo "Safari: ${safari_mem} Mb"
+echo "Chrome: ${chrome_mem} Mb"
+echo "Total: ${total_mem} Mb"
